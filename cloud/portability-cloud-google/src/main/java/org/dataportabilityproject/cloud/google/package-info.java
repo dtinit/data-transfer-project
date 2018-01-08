127f85e76ed9c95460912a215430e7eb61bcd212
Jim Marino <jim.marino@gmail.com>
2018-01-08 11:16:09 +0100

    add cloud extensions modules

---
+++ cloud/portability-cloud-google/src/main/java/org/dataportabilityproject/cloud/google/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Extensions for running the system on Google App Engine.
+ */
+package org.dataportabilityproject.cloud.google;
---
+++ cloud/portability-cloud-microsoft/src/main/java/org/dataportabilityproject/cloud/microsoft/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Extensions for running the system on Microsoft Azure.
+ */
+package org.dataportabilityproject.cloud.microsoft;
