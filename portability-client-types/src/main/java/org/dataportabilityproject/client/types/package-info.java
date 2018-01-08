52e9a0986bd37bfbe037b9cdfbfe7528f3af1e1b
Jim Marino <jim.marino@gmail.com>
2018-01-08 17:05:41 +0100

    add client types subproject

---
+++ portability-client-types/src/main/java/org/dataportabilityproject/client/types/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Defines the data portability client model objects.  Model objects represent the system data resources a client has access to.
+ */
+package org.dataportabilityproject.client.types;
