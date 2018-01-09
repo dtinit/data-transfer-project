ed134d1c46dafbcda087900fb6c445b9821d1065
Jim Marino <jim.marino@gmail.com>
2018-01-09 18:43:03 +0100

    add base data model type

---
+++ portability-data-transfer-types/src/main/java/org/dataportabilityproject/datatransfer/types/models/DataModel.java
@@ -0,0 +1,9 @@
+package org.dataportabilityproject.datatransfer.types.models;
+
+import org.dataportabilityproject.datatransfer.types.PortableType;
+
+/**
+ * The base data model type.
+ */
+public abstract class DataModel extends PortableType {
+}
