bfb1cccfc7a035a24efb8e756b511f8c094cc8cb
Jim Marino <jim.marino@gmail.com>
2018-01-09 15:59:12 +0100

    add Swagger to model types

--- portability-client-types/src/main/java/org/dataportabilityproject/client/types/DataTransferRequest.java
+++ portability-client-types/src/main/java/org/dataportabilityproject/client/types/DataTransferRequest.java
@@ -2,10 +2,13 @@ package org.dataportabilityproject.client.types;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 /**
- * A request to initiate a data transfer.
+ * A request to initiate a data transfer operation.
  */
+@ApiModel(description = "A request to initiate a data transfer operation")
 public class DataTransferRequest {
 
     private String contentType;   // REVIEW: replace old PortableDataType since the latter is an enum and not exctensible?
@@ -15,6 +18,7 @@ public class DataTransferRequest {
         this.contentType = contentType;
     }
 
+    @ApiModelProperty(dataType = "string", required = true)
     public String getContentType() {
         return contentType;
     }
