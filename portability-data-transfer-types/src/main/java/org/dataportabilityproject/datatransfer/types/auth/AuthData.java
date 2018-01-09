422d6100e4653701abfd0f507104b97b4e49e1de
Jim Marino <jim.marino@gmail.com>
2018-01-09 18:27:56 +0100

    add JSON version of auth data

---
+++ portability-data-transfer-types/src/main/java/org/dataportabilityproject/datatransfer/types/auth/AuthData.java
@@ -0,0 +1,9 @@
+package org.dataportabilityproject.datatransfer.types.auth;
+
+import org.dataportabilityproject.datatransfer.types.PortableType;
+
+/**
+ * Base type for authentication data.
+ */
+public abstract class AuthData extends PortableType {
+}
