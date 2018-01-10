ec036c766b45013633b807a60c0f2326a6ef0758
Rachel Tannenbaum <rmtannenbaum@gmail.com>
2018-01-10 15:46:22 -0500

    Test a bad java change
    to make sure it breaks the Travis build (#22)

--- portability-core/src/main/java/org/dataportabilityproject/dataModels/DataModel.java
+++ portability-core/src/main/java/org/dataportabilityproject/dataModels/DataModel.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.dataportabilityproject.dataModels;
+package org.dataportabilityproject.dataModels;THIS_SHOULD_BREAK_THE_BUILD
 
 /**
  * Empty interface to signify an item is a Data Model.
