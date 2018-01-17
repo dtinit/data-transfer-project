5077960adbc61432eff10b341105772d2cec7392
Jim Marino <jim.marino@gmail.com>
2018-01-17 09:04:25 +0100

    Combine JobCache and JobStore

--- portability-spi-cloud/src/main/java/org/dataportabilityproject/spi/cloud/storage/JobStore.java
+++ portability-spi-cloud/src/main/java/org/dataportabilityproject/spi/cloud/storage/JobStore.java
@@ -1,9 +1,12 @@
 package org.dataportabilityproject.spi.cloud.storage;
 
 import org.dataportabilityproject.types.transfer.job.PortabilityJob;
+import org.dataportabilityproject.types.transfer.models.DataModel;
+
+import java.io.InputStream;
 
 /**
- * Implementations handle storage and retrieval of transfer jobs.
+ * Implementations handle storage and retrieval of transfer job data.
  *
  * This class is intended to be implemented by extensions that support storage in various back-end services.
  */
@@ -27,6 +30,26 @@ public interface JobStore {
      * REVIEW: Maybe there should just be state transition-specific methods, e.g cancel(), complete(), etc.?
      */
     void remove(PortabilityJob job);
+
+    /**
+     * Returns a model instance for the id of the given type or null if not found.
+     */
+    <T extends DataModel> T getData(Class<T> type, String id);
+
+    /**
+     * Stores the given model instance associated with a job.
+     */
+    <T extends DataModel> void store(String jobId, T model);
+
+    /**
+     * Stores a stream associated with a job using the given key.
+     */
+    void store(String key, String jobId, InputStream stream);
+
+    /**
+     * Returns stream data for the given key.
+     */
+    InputStream getStream(String key);
 
 
 }
