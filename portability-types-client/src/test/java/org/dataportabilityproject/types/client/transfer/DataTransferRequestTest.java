4e1de78c2d6d36fe444d6900607f55deb2637902
Siham Hussein <sihamh@google.com>
2018-01-25 11:39:19 -0600

    formatting fixes, new lines, license headers

--- portability-types-client/src/test/java/org/dataportabilityproject/types/client/transfer/DataTransferRequestTest.java
+++ portability-types-client/src/test/java/org/dataportabilityproject/types/client/transfer/DataTransferRequestTest.java
@@ -1,24 +1,40 @@
+/*
+* Copyright 2018 The Data-Portability Project Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* https://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package org.dataportabilityproject.types.client.transfer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Test;
 
-/**
- *
- */
+/* Test for DataTransferRequest */
 public class DataTransferRequestTest {
 
-    @Test
-    public void verifySerializeDeserialize() throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
+  @Test
+  public void verifySerializeDeserialize() throws Exception {
+    ObjectMapper objectMapper = new ObjectMapper();
 
-        String serialized = objectMapper.writeValueAsString(new DataTransferRequest("testSource", "testDestination", "application/json"));
+    String serialized = objectMapper.writeValueAsString(
+        new DataTransferRequest("testSource", "testDestination", "application/json"));
 
-        DataTransferRequest deserialized = objectMapper.readValue(serialized, DataTransferRequest.class);
+    DataTransferRequest deserialized = objectMapper
+        .readValue(serialized, DataTransferRequest.class);
 
-        Assert.assertEquals("testSource", deserialized.getSource());
-        Assert.assertEquals("testDestination", deserialized.getDestination());
-        Assert.assertEquals("application/json", deserialized.getTransferDataType());
-    }
+    Assert.assertEquals("testSource", deserialized.getSource());
+    Assert.assertEquals("testDestination", deserialized.getDestination());
+    Assert.assertEquals("application/json", deserialized.getTransferDataType());
+  }
 }
--- portability-types-client/src/test/java/org/dataportabilityproject/types/client/transfer/DataTransferResponseTest.java
+++ portability-types-client/src/test/java/org/dataportabilityproject/types/client/transfer/DataTransferResponseTest.java
@@ -1,23 +1,39 @@
+/*
+* Copyright 2018 The Data-Portability Project Authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* https://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package org.dataportabilityproject.types.client.transfer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Test;
 
-/**
- *
- */
+/* Test for DataTransferResponse */
 public class DataTransferResponseTest {
 
   @Test
   public void verifySerializeDeserialize() throws Exception {
     ObjectMapper objectMapper = new ObjectMapper();
 
-    DataTransferResponse transfer = new DataTransferResponse("testSource", "testDestination", "application/json",
+    DataTransferResponse transfer = new DataTransferResponse("testSource", "testDestination",
+        "application/json",
         DataTransferResponse.Status.INPROCESS, "nexturltofollow");
     String serialized = objectMapper.writeValueAsString(transfer);
 
-    DataTransferResponse deserialized = objectMapper.readValue(serialized, DataTransferResponse.class);
+    DataTransferResponse deserialized = objectMapper
+        .readValue(serialized, DataTransferResponse.class);
 
     Assert.assertEquals(DataTransferResponse.Status.INPROCESS, deserialized.getStatus());
     Assert.assertEquals("nexturltofollow", deserialized.getNextUrl());
