9228f5cfa86b5003851a2b1bfbf9e4370a5860dd
Siham Hussein <seehamrun@users.noreply.github.com>
2018-02-06 09:55:50 -0600

    Start of implementation of PortabilityCopier using new API modules. (#114)
    Start of implementation of PortabilityCopier using new API modules.

--- portability-spi-transfer/src/main/java/org/dataportabilityproject/spi/transfer/provider/TransferServiceProviderRegistry.java
+++ portability-spi-transfer/src/main/java/org/dataportabilityproject/spi/transfer/provider/TransferServiceProviderRegistry.java
@@ -1,15 +1,29 @@
 package org.dataportabilityproject.spi.transfer.provider;
 
+import org.dataportabilityproject.types.transfer.PortableType;
+import org.dataportabilityproject.types.transfer.auth.AuthData;
+import org.dataportabilityproject.types.transfer.models.DataModel;
+
 /**
  * Manages {@link TransferServiceProvider}s registered in the system.
+ * Provides client access methods for Importers and Exporters.
  */
 public interface TransferServiceProviderRegistry {
 
-    /**
-     * Returns the provider that supports the service id.
-     *
-     * @param serviceId the service id
-     */
-    TransferServiceProvider getServiceProvider(String serviceId);
+  /**
+   * Returns the exporter that supports the serviceId and transferDataType.
+   *
+   * @param serviceId the service id
+   * @param transferDataType the transfer data type
+   */
+  Exporter<AuthData, DataModel> getExporter(String serviceId, PortableType transferDataType);
 
+
+  /**
+   * Returns the exporter that supports the serviceId and transferDataType.
+   *
+   * @param serviceId the service id
+   * @param transferDataType the transfer data type
+   */
+  Importer<AuthData, DataModel> getImporter(String serviceId, PortableType transferDataType);
 }
