840f8bdfe43cf87ab22541acf9e792b1e61faaca
Jessie Chavez <chuy@google.com>
2018-03-01 13:17:17 -0600

    formatting fixes

--- portability-spi-cloud/src/main/java/org/dataportabilityproject/spi/cloud/types/JobAuthorization.java
+++ portability-spi-cloud/src/main/java/org/dataportabilityproject/spi/cloud/types/JobAuthorization.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.google.auto.value.AutoValue;
+
 import javax.annotation.Nullable;
 
 /**
@@ -12,94 +13,94 @@ import javax.annotation.Nullable;
 @AutoValue
 @JsonDeserialize(builder = JobAuthorization.Builder.class)
 public abstract class JobAuthorization {
-  /**
-   * The current authorization state of the job.
-   */
-  public enum State {
-    // The job is in the process of obtaining export and import authorization credentials via the
-    // gateway auth flow.
-    INITIAL,
-    // The gateway authorization flow has completed and raw credentials are temporarily available in
-    // the gateway server.
-    CREDS_AVAILABLE,
-    // A worker has spun up and generated a key to encrypt the credentials above so that it (alone)
-    // may use them.
-    CREDS_ENCRYPTION_KEY_GENERATED,
-    // The gateway server has encrypted the credentials for the worker to use.
-    CREDS_ENCRYPTED,
-  }
-
-  @JsonProperty("state")
-  public abstract JobAuthorization.State state();
-
-  @Nullable
-  @JsonProperty("encryptedInitialExportAuthData")
-  public abstract String encryptedInitialExportAuthData();
-
-  @Nullable
-  @JsonProperty("encryptedInitialImportAuthData")
-  public abstract String encryptedInitialImportAuthData();
-
-  @Nullable
-  @JsonProperty("encryptedExportAuthData")
-  public abstract String encryptedExportAuthData();
-
-  @Nullable
-  @JsonProperty("encryptedImportAuthData")
-  public abstract String encryptedImportAuthData();
-
-  @JsonProperty("encryptedSessionKey")
-  public abstract String encryptedSessionKey();
-
-  @Nullable
-  @JsonProperty("encryptedPublicKey")
-  public abstract String encryptedPublicKey();
-
-  @Nullable
-  @JsonProperty("encryptedPrivateKey")
-  public abstract String encryptedPrivateKey();
-
-  public static Builder builder() {
-    // TODO: Fix so we don't need fully qualified name here. This is to get IntelliJ to recognize
-    // the class name due to a conflict in package names for our generated code, but the conflict
-    // doesn't cause any actual problems with building.
-    return new org.dataportabilityproject.spi.cloud.types.AutoValue_JobAuthorization.Builder()
-        .setState(State.INITIAL);
-  }
-
-  public abstract Builder toBuilder();
-
-  @AutoValue.Builder
-  public abstract static class Builder {
-    @JsonCreator
-    private static Builder create() {
-      return JobAuthorization.builder();
+    /**
+     * The current authorization state of the job.
+     */
+    public enum State {
+        // The job is in the process of obtaining export and import authorization credentials via the
+        // gateway auth flow.
+        INITIAL,
+        // The gateway authorization flow has completed and raw credentials are temporarily available in
+        // the gateway server.
+        CREDS_AVAILABLE,
+        // A worker has spun up and generated a key to encrypt the credentials above so that it (alone)
+        // may use them.
+        CREDS_ENCRYPTION_KEY_GENERATED,
+        // The gateway server has encrypted the credentials for the worker to use.
+        CREDS_ENCRYPTED,
     }
 
     @JsonProperty("state")
-    public abstract Builder setState(JobAuthorization.State state);
+    public abstract JobAuthorization.State state();
 
+    @Nullable
     @JsonProperty("encryptedInitialExportAuthData")
-    public abstract Builder setEncryptedInitialExportAuthData(String authData);
+    public abstract String encryptedInitialExportAuthData();
 
+    @Nullable
     @JsonProperty("encryptedInitialImportAuthData")
-    public abstract Builder setEncryptedInitialImportAuthData(String authData);
+    public abstract String encryptedInitialImportAuthData();
 
+    @Nullable
     @JsonProperty("encryptedExportAuthData")
-    public abstract Builder setEncryptedExportAuthData(String authData);
+    public abstract String encryptedExportAuthData();
 
+    @Nullable
     @JsonProperty("encryptedImportAuthData")
-    public abstract Builder setEncryptedImportAuthData(String authData);
+    public abstract String encryptedImportAuthData();
 
     @JsonProperty("encryptedSessionKey")
-    public abstract Builder setEncryptedSessionKey(String sessionKey);
+    public abstract String encryptedSessionKey();
 
+    @Nullable
     @JsonProperty("encryptedPublicKey")
-    public abstract Builder setEncryptedPublicKey(String publicKey);
+    public abstract String encryptedPublicKey();
 
+    @Nullable
     @JsonProperty("encryptedPrivateKey")
-    public abstract Builder setEncryptedPrivateKey(String privateKey);
+    public abstract String encryptedPrivateKey();
 
-    public abstract JobAuthorization build();
-  }
+    public static Builder builder() {
+        // TODO: Fix so we don't need fully qualified name here. This is to get IntelliJ to recognize
+        // the class name due to a conflict in package names for our generated code, but the conflict
+        // doesn't cause any actual problems with building.
+        return new org.dataportabilityproject.spi.cloud.types.AutoValue_JobAuthorization.Builder()
+                .setState(State.INITIAL);
+    }
+
+    public abstract Builder toBuilder();
+
+    @AutoValue.Builder
+    public abstract static class Builder {
+        @JsonCreator
+        private static Builder create() {
+            return JobAuthorization.builder();
+        }
+
+        @JsonProperty("state")
+        public abstract Builder setState(JobAuthorization.State state);
+
+        @JsonProperty("encryptedInitialExportAuthData")
+        public abstract Builder setEncryptedInitialExportAuthData(String authData);
+
+        @JsonProperty("encryptedInitialImportAuthData")
+        public abstract Builder setEncryptedInitialImportAuthData(String authData);
+
+        @JsonProperty("encryptedExportAuthData")
+        public abstract Builder setEncryptedExportAuthData(String authData);
+
+        @JsonProperty("encryptedImportAuthData")
+        public abstract Builder setEncryptedImportAuthData(String authData);
+
+        @JsonProperty("encryptedSessionKey")
+        public abstract Builder setEncryptedSessionKey(String sessionKey);
+
+        @JsonProperty("encryptedPublicKey")
+        public abstract Builder setEncryptedPublicKey(String publicKey);
+
+        @JsonProperty("encryptedPrivateKey")
+        public abstract Builder setEncryptedPrivateKey(String privateKey);
+
+        public abstract JobAuthorization build();
+    }
 }
--- portability-worker/src/main/java/org/dataportabilityproject/worker/JobPollingService.java
+++ portability-worker/src/main/java/org/dataportabilityproject/worker/JobPollingService.java
@@ -19,12 +19,14 @@ import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import com.google.common.util.concurrent.AbstractScheduledService;
 import com.google.inject.Inject;
+
 import java.io.IOException;
 import java.security.KeyPair;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+
 import org.dataportabilityproject.cloud.interfaces.CloudFactory;
 import org.dataportabilityproject.job.PublicPrivateKeyPairGenerator;
 import org.dataportabilityproject.spi.cloud.storage.JobStore;
@@ -36,112 +38,112 @@ import org.slf4j.LoggerFactory;
 /**
  * A service that polls storage for a job to process in two steps:
  * <br>
- *   (1) find an unassigned job for this worker
+ * (1) find an unassigned job for this worker
  * <br>
- *   (2) wait until the job is ready to process (i.e. creds are available)
+ * (2) wait until the job is ready to process (i.e. creds are available)
  */
 class JobPollingService extends AbstractScheduledService {
-  private final Logger logger = LoggerFactory.getLogger(JobPollingService.class);
-  private final JobStore store;
-  private final WorkerJobMetadata jobMetadata;
+    private final Logger logger = LoggerFactory.getLogger(JobPollingService.class);
+    private final JobStore store;
+    private final WorkerJobMetadata jobMetadata;
 
-  @Inject
-  JobPollingService(CloudFactory cloudFactory, WorkerJobMetadata jobMetadata) {
-    this.store = cloudFactory.getJobStore();
-    this.jobMetadata = jobMetadata;
-  }
-
-  @Override
-  protected void runOneIteration() throws Exception {
-    if (jobMetadata.isInitialized()) {
-      pollUntilJobIsReady();
-    } else {
-      // Poll for an unassigned job to process with this worker instance.
-      // Once a worker instance is assigned, the client will populate storage with
-      // auth data encrypted with this instances public key and the copy process can begin
-      pollForUnassignedJob();
+    @Inject
+    JobPollingService(CloudFactory cloudFactory, WorkerJobMetadata jobMetadata) {
+        this.store = cloudFactory.getJobStore();
+        this.jobMetadata = jobMetadata;
     }
-  }
 
-  @Override
-  protected Scheduler scheduler() {
-    return AbstractScheduledService.Scheduler.newFixedDelaySchedule(0, 20, TimeUnit.SECONDS);
-  }
-
-  /**
-   * Polls for an unassigned job, and once found, initializes the global singleton job metadata
-   * object for this running instance of the worker.
-   */
-  private void pollForUnassignedJob() throws IOException {
-    UUID jobId = store.findFirst(JobAuthorization.State.CREDS_AVAILABLE);
-    logger.debug("Polling for a job CREDS_AVAILABLE");
-    if (jobId == null) {
-      return;
+    @Override
+    protected void runOneIteration() throws Exception {
+        if (jobMetadata.isInitialized()) {
+            pollUntilJobIsReady();
+        } else {
+            // Poll for an unassigned job to process with this worker instance.
+            // Once a worker instance is assigned, the client will populate storage with
+            // auth data encrypted with this instances public key and the copy process can begin
+            pollForUnassignedJob();
+        }
     }
-    logger.debug("Polled job {}", jobId);
-    Preconditions.checkState(!jobMetadata.isInitialized());
-    KeyPair keyPair = PublicPrivateKeyPairGenerator.generateKeyPair();
-    PublicKey publicKey = keyPair.getPublic();
-    // TODO: Move storage of private key to a different location
-    PrivateKey privateKey = keyPair.getPrivate();
-    // Executing Job State Transition from Unassigned (auth state INITIAL) to Assigned (auth state
-    // CREDS_ENCRYPTION_KEY_GENERATED).
-    try {
-      keyGenerated(jobId, publicKey, privateKey);
-      jobMetadata.init(jobId, keyPair);
-      logger.debug("Updated job {} to CREDS_ENCRYPTION_KEY_GENERATED, publicKey length: {}",
-          jobId, publicKey.getEncoded().length);
-    } catch (IOException e) {
-      logger.debug("Failed to claim job {}; it was probably already claimed by another worker",
-          jobId);
+
+    @Override
+    protected Scheduler scheduler() {
+        return AbstractScheduledService.Scheduler.newFixedDelaySchedule(0, 20, TimeUnit.SECONDS);
     }
-  }
 
-  /**
-   * Updates a unassigned {@link PortabilityJob} in storage with the provided {@code jobId} in
-   * CREDS_ENCRYPTION_KEY_GENERATED state with {@code publicKey} and {@code privateKey}.
-   */
-  private void keyGenerated(UUID jobId, PublicKey publicKey, PrivateKey privateKey)
-      throws IOException {
-    // Lookup the job so we can append to its existing properties.
-    PortabilityJob existingJob = store.findJob(jobId);
-    // Verify no worker key
-    // Populate job with keys to persist
-    String encodedPublicKey = PublicPrivateKeyPairGenerator.encodeKey(publicKey);
-    String encodedPrivateKey = PublicPrivateKeyPairGenerator.encodeKey(privateKey);
-
-    PortabilityJob updatedJob = existingJob.toBuilder()
-        .setAndValidateJobAuthorization(existingJob.jobAuthorization().toBuilder()
-            .setEncryptedPublicKey(encodedPublicKey)
-            .setEncryptedPrivateKey(encodedPrivateKey)
-            .setState(JobAuthorization.State.CREDS_ENCRYPTION_KEY_GENERATED)
-            .build())
-        .build();
-    store.updateJob(jobId, updatedJob);
-  }
-
-  /**
-   * Polls for job with populated auth data and stops this service when found.
-   */
-  private void pollUntilJobIsReady() {
-    UUID jobId = jobMetadata.getJobId();
-    PortabilityJob job = store.findJob(jobId);
-    if (job == null) {
-      logger.debug("Could not poll job {}, it was not present in the key-value store", jobId);
-    } else if (job.jobAuthorization().state() == JobAuthorization.State.CREDS_ENCRYPTED) {
-      logger.debug("Polled job {} in state CREDS_ENCRYPTED", jobId);
-      JobAuthorization jobAuthorization = job.jobAuthorization();
-      if (!Strings.isNullOrEmpty(jobAuthorization.encryptedExportAuthData())
-          && !Strings.isNullOrEmpty(jobAuthorization.encryptedImportAuthData())) {
-        logger.debug("Polled job {} has auth data as expected. Done polling.", jobId);
-      } else {
-        logger.warn("Polled job {} does not have auth data as expected. "
-            + "Done polling this job since it's in a bad state! Starting over.", jobId);
-      }
-      this.stopAsync();
-    } else {
-      logger.debug("Polling job {} until it's in state CREDS_ENCRYPTED. "
-          + "It's currently in state: {}", jobId, job.jobAuthorization().state());
+    /**
+     * Polls for an unassigned job, and once found, initializes the global singleton job metadata
+     * object for this running instance of the worker.
+     */
+    private void pollForUnassignedJob() throws IOException {
+        UUID jobId = store.findFirst(JobAuthorization.State.CREDS_AVAILABLE);
+        logger.debug("Polling for a job CREDS_AVAILABLE");
+        if (jobId == null) {
+            return;
+        }
+        logger.debug("Polled job {}", jobId);
+        Preconditions.checkState(!jobMetadata.isInitialized());
+        KeyPair keyPair = PublicPrivateKeyPairGenerator.generateKeyPair();
+        PublicKey publicKey = keyPair.getPublic();
+        // TODO: Move storage of private key to a different location
+        PrivateKey privateKey = keyPair.getPrivate();
+        // Executing Job State Transition from Unassigned (auth state INITIAL) to Assigned (auth state
+        // CREDS_ENCRYPTION_KEY_GENERATED).
+        try {
+            keyGenerated(jobId, publicKey, privateKey);
+            jobMetadata.init(jobId, keyPair);
+            logger.debug("Updated job {} to CREDS_ENCRYPTION_KEY_GENERATED, publicKey length: {}",
+                    jobId, publicKey.getEncoded().length);
+        } catch (IOException e) {
+            logger.debug("Failed to claim job {}; it was probably already claimed by another worker",
+                    jobId);
+        }
     }
-  }
+
+    /**
+     * Updates a unassigned {@link PortabilityJob} in storage with the provided {@code jobId} in
+     * CREDS_ENCRYPTION_KEY_GENERATED state with {@code publicKey} and {@code privateKey}.
+     */
+    private void keyGenerated(UUID jobId, PublicKey publicKey, PrivateKey privateKey)
+            throws IOException {
+        // Lookup the job so we can append to its existing properties.
+        PortabilityJob existingJob = store.findJob(jobId);
+        // Verify no worker key
+        // Populate job with keys to persist
+        String encodedPublicKey = PublicPrivateKeyPairGenerator.encodeKey(publicKey);
+        String encodedPrivateKey = PublicPrivateKeyPairGenerator.encodeKey(privateKey);
+
+        PortabilityJob updatedJob = existingJob.toBuilder()
+                .setAndValidateJobAuthorization(existingJob.jobAuthorization().toBuilder()
+                        .setEncryptedPublicKey(encodedPublicKey)
+                        .setEncryptedPrivateKey(encodedPrivateKey)
+                        .setState(JobAuthorization.State.CREDS_ENCRYPTION_KEY_GENERATED)
+                        .build())
+                .build();
+        store.updateJob(jobId, updatedJob);
+    }
+
+    /**
+     * Polls for job with populated auth data and stops this service when found.
+     */
+    private void pollUntilJobIsReady() {
+        UUID jobId = jobMetadata.getJobId();
+        PortabilityJob job = store.findJob(jobId);
+        if (job == null) {
+            logger.debug("Could not poll job {}, it was not present in the key-value store", jobId);
+        } else if (job.jobAuthorization().state() == JobAuthorization.State.CREDS_ENCRYPTED) {
+            logger.debug("Polled job {} in state CREDS_ENCRYPTED", jobId);
+            JobAuthorization jobAuthorization = job.jobAuthorization();
+            if (!Strings.isNullOrEmpty(jobAuthorization.encryptedExportAuthData())
+                    && !Strings.isNullOrEmpty(jobAuthorization.encryptedImportAuthData())) {
+                logger.debug("Polled job {} has auth data as expected. Done polling.", jobId);
+            } else {
+                logger.warn("Polled job {} does not have auth data as expected. "
+                        + "Done polling this job since it's in a bad state! Starting over.", jobId);
+            }
+            this.stopAsync();
+        } else {
+            logger.debug("Polling job {} until it's in state CREDS_ENCRYPTED. "
+                    + "It's currently in state: {}", jobId, job.jobAuthorization().state());
+        }
+    }
 }
