bed68abbb54bbce66269db036ea31f40f76f3771
Siham Hussein <seehamrun@users.noreply.github.com>
2018-03-06 13:20:35 -0600

    reformat portability-spi-cloud directory (#177)

--- portability-spi-cloud/src/main/java/org/dataportabilityproject/spi/cloud/types/JobAuthorization.java
+++ portability-spi-cloud/src/main/java/org/dataportabilityproject/spi/cloud/types/JobAuthorization.java
@@ -4,96 +4,91 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.google.auto.value.AutoValue;
-
 import javax.annotation.Nullable;
 
-/**
- * Authorization data related to a job.
- */
+/** Authorization data related to a job. */
 @AutoValue
 @JsonDeserialize(builder = JobAuthorization.Builder.class)
 public abstract class JobAuthorization {
-    /**
-     * The current authorization state of the job.
-     */
-    public enum State {
-        // The job is in the process of obtaining export and import authorization credentials via the
-        // gateway auth flow.
-        INITIAL,
-        // The gateway authorization flow has completed and raw credentials are temporarily available in
-        // the gateway server.
-        CREDS_AVAILABLE,
-        // A worker has spun up and generated a key to encrypt the credentials above so that it (alone)
-        // may use them.
-        CREDS_ENCRYPTION_KEY_GENERATED,
-        // The gateway server has encrypted the credentials for the worker to use.
-        CREDS_ENCRYPTED,
+  public static Builder builder() {
+    // TODO: Fix so we don't need fully qualified name here. This is to get IntelliJ to recognize
+    // the class name due to a conflict in package names for our generated code, but the conflict
+    // doesn't cause any actual problems with building.
+    return new org.dataportabilityproject.spi.cloud.types.AutoValue_JobAuthorization.Builder()
+        .setState(State.INITIAL);
+  }
+
+  @JsonProperty("state")
+  public abstract JobAuthorization.State state();
+
+  @Nullable
+  @JsonProperty("encryptedInitialExportAuthData")
+  public abstract String encryptedInitialExportAuthData();
+
+  @Nullable
+  @JsonProperty("encryptedInitialImportAuthData")
+  public abstract String encryptedInitialImportAuthData();
+
+  @Nullable
+  @JsonProperty("encryptedExportAuthData")
+  public abstract String encryptedExportAuthData();
+
+  @Nullable
+  @JsonProperty("encryptedImportAuthData")
+  public abstract String encryptedImportAuthData();
+
+  @JsonProperty("encryptedSessionKey")
+  public abstract String encryptedSessionKey();
+
+  @Nullable
+  @JsonProperty("encodedPublicKey")
+  public abstract String encodedPublicKey();
+
+  public abstract Builder toBuilder();
+
+  /** The current authorization state of the job. */
+  public enum State {
+    // The job is in the process of obtaining export and import authorization credentials via the
+    // gateway auth flow.
+    INITIAL,
+    // The gateway authorization flow has completed and raw credentials are temporarily available in
+    // the gateway server.
+    CREDS_AVAILABLE,
+    // A worker has spun up and generated a key to encrypt the credentials above so that it (alone)
+    // may use them.
+    CREDS_ENCRYPTION_KEY_GENERATED,
+    // The gateway server has encrypted the credentials for the worker to use.
+    CREDS_ENCRYPTED,
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    @JsonCreator
+    private static Builder create() {
+      return JobAuthorization.builder();
     }
 
     @JsonProperty("state")
-    public abstract JobAuthorization.State state();
+    public abstract Builder setState(JobAuthorization.State state);
 
-    @Nullable
     @JsonProperty("encryptedInitialExportAuthData")
-    public abstract String encryptedInitialExportAuthData();
+    public abstract Builder setEncryptedInitialExportAuthData(String authData);
 
-    @Nullable
     @JsonProperty("encryptedInitialImportAuthData")
-    public abstract String encryptedInitialImportAuthData();
+    public abstract Builder setEncryptedInitialImportAuthData(String authData);
 
-    @Nullable
     @JsonProperty("encryptedExportAuthData")
-    public abstract String encryptedExportAuthData();
+    public abstract Builder setEncryptedExportAuthData(String authData);
 
-    @Nullable
     @JsonProperty("encryptedImportAuthData")
-    public abstract String encryptedImportAuthData();
+    public abstract Builder setEncryptedImportAuthData(String authData);
 
     @JsonProperty("encryptedSessionKey")
-    public abstract String encryptedSessionKey();
+    public abstract Builder setEncryptedSessionKey(String sessionKey);
 
-    @Nullable
     @JsonProperty("encodedPublicKey")
-    public abstract String encodedPublicKey();
+    public abstract Builder setEncodedPublicKey(String publicKey);
 
-    public static Builder builder() {
-        // TODO: Fix so we don't need fully qualified name here. This is to get IntelliJ to recognize
-        // the class name due to a conflict in package names for our generated code, but the conflict
-        // doesn't cause any actual problems with building.
-        return new org.dataportabilityproject.spi.cloud.types.AutoValue_JobAuthorization.Builder()
-                .setState(State.INITIAL);
-    }
-
-    public abstract Builder toBuilder();
-
-    @AutoValue.Builder
-    public abstract static class Builder {
-        @JsonCreator
-        private static Builder create() {
-            return JobAuthorization.builder();
-        }
-
-        @JsonProperty("state")
-        public abstract Builder setState(JobAuthorization.State state);
-
-        @JsonProperty("encryptedInitialExportAuthData")
-        public abstract Builder setEncryptedInitialExportAuthData(String authData);
-
-        @JsonProperty("encryptedInitialImportAuthData")
-        public abstract Builder setEncryptedInitialImportAuthData(String authData);
-
-        @JsonProperty("encryptedExportAuthData")
-        public abstract Builder setEncryptedExportAuthData(String authData);
-
-        @JsonProperty("encryptedImportAuthData")
-        public abstract Builder setEncryptedImportAuthData(String authData);
-
-        @JsonProperty("encryptedSessionKey")
-        public abstract Builder setEncryptedSessionKey(String sessionKey);
-
-        @JsonProperty("encodedPublicKey")
-        public abstract Builder setEncodedPublicKey(String publicKey);
-
-        public abstract JobAuthorization build();
-    }
+    public abstract JobAuthorization build();
+  }
 }
