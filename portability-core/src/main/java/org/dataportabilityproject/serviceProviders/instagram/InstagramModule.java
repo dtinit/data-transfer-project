bc8176de7d5b04d768833c3e65d3313df22e7927
Siham Hussein <seehamrun@users.noreply.github.com>
2018-03-09 10:53:22 -0600

    Change author in copy right from Data-Portability Project to Data Transfer Project(#196)

--- portability-core/src/main/java/org/dataportabilityproject/serviceProviders/instagram/InstagramModule.java
+++ portability-core/src/main/java/org/dataportabilityproject/serviceProviders/instagram/InstagramModule.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 The Data-Portability Project Authors.
+ * Copyright 2017 The Data Transfer Project Authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
