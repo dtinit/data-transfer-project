2b35e4b46bc5c9c4caef28f7a7de56f14885c0a8
Anna Olson <olsona@google.com>
2018-03-13 17:18:57 -0500

    License headers

--- extensions/data-transfer/portability-data-transfer-google/src/test/java/org/dataportabilityproject/datatransfer/google/contacts/GoogleContactsExporterTest.java
+++ extensions/data-transfer/portability-data-transfer-google/src/test/java/org/dataportabilityproject/datatransfer/google/contacts/GoogleContactsExporterTest.java
@@ -1,9 +1,24 @@
+/*
+ * Copyright 2018 The Data Transfer Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.dataportabilityproject.datatransfer.google.contacts;
 
 import static com.google.common.truth.Truth.assertThat;
 import static org.dataportabilityproject.datatransfer.google.common.GoogleStaticObjects.PERSON_FIELDS;
 import static org.dataportabilityproject.datatransfer.google.common.GoogleStaticObjects.SELF_RESOURCE;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -11,7 +26,6 @@ import static org.mockito.Mockito.when;
 import com.google.api.services.people.v1.PeopleService;
 import com.google.api.services.people.v1.PeopleService.People;
 import com.google.api.services.people.v1.PeopleService.People.Connections;
-import com.google.api.services.people.v1.PeopleService.People.CreateContact;
 import com.google.api.services.people.v1.PeopleService.People.GetBatchGet;
 import com.google.api.services.people.v1.model.FieldMetadata;
 import com.google.api.services.people.v1.model.GetPeopleResponse;
--- extensions/data-transfer/portability-data-transfer-google/src/test/java/org/dataportabilityproject/datatransfer/google/contacts/GoogleContactsImporterTest.java
+++ extensions/data-transfer/portability-data-transfer-google/src/test/java/org/dataportabilityproject/datatransfer/google/contacts/GoogleContactsImporterTest.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright 2018 The Data Transfer Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.dataportabilityproject.datatransfer.google.contacts;
 
 import static org.dataportabilityproject.datatransfer.google.common.GoogleStaticObjects.CONTACT_SOURCE_TYPE;
-import static org.dataportabilityproject.datatransfer.google.common.GoogleStaticObjects.PERSON_FIELDS;
 import static org.dataportabilityproject.datatransfer.google.common.GoogleStaticObjects.SOURCE_PARAM_NAME_TYPE;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
@@ -11,19 +26,11 @@ import static org.mockito.Mockito.when;
 
 import com.google.api.services.people.v1.PeopleService;
 import com.google.api.services.people.v1.PeopleService.People;
-import com.google.api.services.people.v1.PeopleService.People.Connections;
 import com.google.api.services.people.v1.PeopleService.People.CreateContact;
-import com.google.api.services.people.v1.PeopleService.People.GetBatchGet;
-import com.google.api.services.people.v1.model.FieldMetadata;
-import com.google.api.services.people.v1.model.ListConnectionsResponse;
-import com.google.api.services.people.v1.model.Name;
 import com.google.api.services.people.v1.model.Person;
-import com.google.api.services.people.v1.model.Source;
 import ezvcard.VCard;
 import ezvcard.property.StructuredName;
 import java.io.IOException;
-import java.io.StringWriter;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import org.dataportabilityproject.types.transfer.models.contacts.ContactsModelWrapper;
@@ -31,14 +38,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class GoogleContactsImporterTest {
-  private static final String RESOURCE_NAME = "resource_name";
-  private static final Source SOURCE = new Source().setType("CONTACT");
-  private static final FieldMetadata PRIMARY_FIELD_METADATA =
-      new FieldMetadata().setSource(SOURCE).setPrimary(true);
-  private static final Name NAME =
-      new Name().setFamilyName("Turing").setGivenName("Alan").setMetadata(PRIMARY_FIELD_METADATA);
-  private static final Person PERSON =
-      new Person().setNames(Collections.singletonList(NAME)).setResourceName(RESOURCE_NAME);
 
   private PeopleService peopleService;
   private GoogleContactsImporter contactsService;
