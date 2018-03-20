6c37523ae11f12adca876687789a222d59175513
Siham Hussein <sihamh@google.com>
2018-03-20 13:48:06 -0500

    dummy impl of FlickrPhotosImporter

--- extensions/data-transfer/portability-data-transfer-flickr/src/main/java/org/dataportabilityproject/datatransfer/flickr/photos/FlickrPhotosImporter.java
+++ extensions/data-transfer/portability-data-transfer-flickr/src/main/java/org/dataportabilityproject/datatransfer/flickr/photos/FlickrPhotosImporter.java
@@ -16,15 +16,63 @@
 
 package org.dataportabilityproject.datatransfer.flickr.photos;
 
+import com.flickr4java.flickr.FlickrException;
+import com.flickr4java.flickr.uploader.UploadMetaData;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import org.dataportabilityproject.spi.cloud.storage.JobStore;
 import org.dataportabilityproject.spi.transfer.provider.ImportResult;
 import org.dataportabilityproject.spi.transfer.provider.Importer;
 import org.dataportabilityproject.types.transfer.auth.AuthData;
+import org.dataportabilityproject.types.transfer.models.photos.PhotoAlbum;
+import org.dataportabilityproject.types.transfer.models.photos.PhotoModel;
 import org.dataportabilityproject.types.transfer.models.photos.PhotosContainerResource;
 
 public class FlickrPhotosImporter implements Importer<AuthData, PhotosContainerResource> {
+  private final JobStore jobStore;
+
+  FlickrPhotosImporter(JobStore jobStore){
+    this.jobStore = jobStore;
+  }
 
   @Override
   public ImportResult importItem(String jobId, AuthData authData, PhotosContainerResource data) {
+    for(PhotoAlbum album : data.getAlbums()) {
+      String key = album.getId();
+      // TODO: store key and album into the JobStore
+    }
+
+    for (PhotoModel photo : data.getPhotos()) {
+      try{
+      String photoId = uploadPhoto(photo);
+      } catch(FlickrException | IOException e) {
+        throw new IllegalArgumentException(e);
+      }
+
+      String oldPhotoId = photo.getAlbumId();
+
+      if(/*!jobStore.hasKey(old album)*/false) {
+        // get data for albummetadata+oldalbum id
+        // use photosetsinterface to create the prefix of the albmu
+        // store the old album id into the jobdata cache
+      } else {
+        // old alsums created in the cache, add the photo to the album
+      }
+    }
     return null;
   }
+
+  private String uploadPhoto(PhotoModel photo) throws IOException, FlickrException {
+    BufferedInputStream inStream ;//= imageStreamProvider.get(photo.getFetchableUrl());
+    UploadMetaData uploadMetaData =
+        new UploadMetaData()
+            .setAsync(false)
+            .setPublicFlag(false)
+            .setFriendFlag(false)
+            .setFamilyFlag(false)
+            .setTitle(/*COPY_PREFIX + */ photo.getTitle())
+            .setDescription(photo.getDescription());
+    return "blah"; // uploader.upload(inStream, uploadMetaData);
+  }
+
 }
