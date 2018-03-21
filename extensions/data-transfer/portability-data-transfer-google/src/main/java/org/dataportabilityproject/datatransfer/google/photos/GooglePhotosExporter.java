7c657fde4b425edef5c49542af012485c96ab7e1
Anna Olson <olsona@google.com>
2018-03-20 21:21:36 -0500

    Basics for exporter

--- extensions/data-transfer/portability-data-transfer-google/src/main/java/org/dataportabilityproject/datatransfer/google/photos/GooglePhotosExporter.java
+++ extensions/data-transfer/portability-data-transfer-google/src/main/java/org/dataportabilityproject/datatransfer/google/photos/GooglePhotosExporter.java
@@ -18,19 +18,31 @@ package org.dataportabilityproject.datatransfer.google.photos;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.gdata.client.photos.PicasawebService;
+import com.google.gdata.data.MediaContent;
+import com.google.gdata.data.photos.AlbumFeed;
+import com.google.gdata.data.photos.GphotoEntry;
 import com.google.gdata.data.photos.UserFeed;
 import com.google.gdata.util.ServiceException;
 import org.dataportabilityproject.datatransfer.google.common.GoogleStaticObjects;
 import org.dataportabilityproject.spi.cloud.storage.JobStore;
 import org.dataportabilityproject.spi.transfer.provider.ExportResult;
 import org.dataportabilityproject.spi.transfer.provider.Exporter;
+import org.dataportabilityproject.spi.transfer.provider.ExportResult.ResultType;
+import org.dataportabilityproject.spi.transfer.types.ContinuationData;
 import org.dataportabilityproject.spi.transfer.types.ExportInformation;
+import org.dataportabilityproject.spi.transfer.types.IdOnlyContainerResource;
 import org.dataportabilityproject.spi.transfer.types.PaginationData;
 import org.dataportabilityproject.types.transfer.auth.AuthData;
+import org.dataportabilityproject.types.transfer.models.ContainerResource;
+import org.dataportabilityproject.types.transfer.models.photos.PhotoAlbum;
+import org.dataportabilityproject.types.transfer.models.photos.PhotoModel;
 import org.dataportabilityproject.types.transfer.models.photos.PhotosContainerResource;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class GooglePhotosExporter implements Exporter<AuthData, PhotosContainerResource> {
@@ -59,14 +71,58 @@ public class GooglePhotosExporter implements Exporter<AuthData, PhotosContainerR
     return null;
   }
 
-  private ExportResult<PhotosContainerResource> exportAlbums(
-          AuthData authData, Optional<PaginationData> paginationData) {
+  private ExportResult<PhotosContainerResource> exportAlbums(AuthData authData) {
+    // TODO(olsona): is pagination data relevant for this?
+
     try {
       URL albumUrl = new URL("https://picasaweb.google.com/data/feed/api/user/default?kind=album");
+      UserFeed albumFeed = getOrCreatePhotosService(authData).getFeed(albumUrl, UserFeed.class);
 
-      UserFeed albumFeed = service.getFeed(albumUrl, UserFeed.class);
-    } catch (ServiceException e) {
-      throw new IOException("Problem making request to: " + albumUrl, e);
+      List<PhotoAlbum> albums = new ArrayList<>(albumFeed.getAlbumEntries().size());
+      ContinuationData continuationData = new ContinuationData(null);
+
+      for (GphotoEntry googleAlbum : albumFeed.getAlbumEntries()) {
+        // Add album info to list so album can be recreated later
+        albums.add(new PhotoAlbum(googleAlbum.getGphotoId(), googleAlbum.getTitle().getPlainText(),
+                googleAlbum.getDescription().getPlainText()));
+
+        // Add album id to continuation data
+        continuationData.addContainerResource(new IdOnlyContainerResource(googleAlbum.getGphotoId()));
+      }
+
+      ResultType resultType = ResultType.CONTINUE;
+      PhotosContainerResource containerResource = new PhotosContainerResource(albums, null);
+      return new ExportResult<PhotosContainerResource>
+              (resultType, containerResource, continuationData);
+    } catch (ServiceException | IOException e) {
+      return new ExportResult<PhotosContainerResource>(ResultType.ERROR, e.getMessage());
+    }
+  }
+
+  private ExportResult<PhotosContainerResource> exportPhotos(String albumId, AuthData authData) {
+    // TODO(olsona): pagination data?
+
+    try {
+      // imgmax=d gets the original image as per:
+      // https://developers.google.com/picasa-web/docs/3.0/reference
+      URL photosUrl = new URL(
+              "https://picasaweb.google.com/data/feed/api/user/default/albumid/"
+                      + albumId
+                      + "?imgmax=d");
+      AlbumFeed photoFeed = getOrCreatePhotosService(authData).getFeed(photosUrl, AlbumFeed.class);
+
+      List<PhotoModel> photos = new ArrayList<>(photoFeed.getEntries().size());
+      for (GphotoEntry photo : photoFeed.getEntries()) {
+        MediaContent mediaContent = (MediaContent) photo.getContent();
+        photos.add(new PhotoModel(photo.getTitle().getPlainText(), mediaContent.getUri(), photo
+                .getDescription().getPlainText(), mediaContent.getMimeType().getMediaType(),
+                albumId));
+      }
+
+      PhotosContainerResource containerResource = new PhotosContainerResource(null, photos);
+      return new ExportResult<PhotosContainerResource>(ResultType.END, containerResource, null);
+    } catch (ServiceException | IOException e) {
+      return new ExportResult<PhotosContainerResource>(ResultType.ERROR, e.getMessage());
     }
   }
 
