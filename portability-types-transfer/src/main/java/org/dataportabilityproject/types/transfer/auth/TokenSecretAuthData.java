d0aa6709cb1b035f2460fd257ae660cbce88016e
Siham Hussein <seehamrun@users.noreply.github.com>
2018-03-21 17:15:44 -0500

    Implement FlickrPhotosExporter (#279)
    * flickr photos exporter
    * add portability-data-transfer-flickr to settings.gradle
    * return error result type instead of throwing exception

---
+++ portability-types-transfer/src/main/java/org/dataportabilityproject/types/transfer/auth/TokenSecretAuthData.java
@@ -0,0 +1,28 @@
+package org.dataportabilityproject.types.transfer.auth;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
+/** Token-secret-based authentication data. */
+@JsonTypeName("org.dataportability:TokenSecretAuthData")
+public class TokenSecretAuthData extends AuthData {
+  private final String token;
+  private final String secret;
+
+  @JsonCreator
+  public TokenSecretAuthData(
+      @JsonProperty("token") String token,
+      @JsonProperty("secret") String secret) {
+    this.token = token;
+    this.secret = secret;
+  }
+
+  public String getToken() {
+    return token;
+  }
+
+  public String getSecret() {
+    return secret;
+  }
+}
