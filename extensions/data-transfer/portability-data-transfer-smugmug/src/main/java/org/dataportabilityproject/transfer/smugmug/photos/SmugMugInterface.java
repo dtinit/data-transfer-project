2dc48edef1a271727c63a80c721e2e81c5fec316
Anna Olson <olsona@google.com>
2018-03-22 16:07:30 -0500

    Basics and some infrastructure for SmugMug, starting exporter

---
+++ extensions/data-transfer/portability-data-transfer-smugmug/src/main/java/org/dataportabilityproject/transfer/smugmug/photos/SmugMugInterface.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2018 The Data Transfer Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dataportabilityproject.transfer.smugmug.photos;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpContent;
+import com.google.api.client.http.HttpHeaders;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.Map.Entry;
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
+import org.dataportabilityproject.transfer.smugmug.photos.model.SmugMugAlbumInfoResponse;
+import org.dataportabilityproject.transfer.smugmug.photos.model.SmugMugResponse;
+import org.dataportabilityproject.transfer.smugmug.photos.model.SmugMugUserResponse;
+import org.dataportabilityproject.transfer.smugmug.photos.model.SmugMugAlbumsResponse;
+
+public class SmugMugInterface {
+
+  // TODO(olsona): is this the mapper we want to use?
+  static final ObjectMapper MAPPER =
+      new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+  static final String BASE_URL = "https://api.smugmug.com";
+  static final String USER_URL = "/api/v2!authuser";
+  static final String ALBUMS_KEY = "UserAlbums";
+
+  // TODO(olsona): do we want to use the Signpost OAuth library?
+  private final OAuthConsumer authConsumer;
+  private final HttpTransport httpTransport;
+
+  // TODO(olsona): this!
+  SmugMugInterface() {
+    this.authConsumer = null;
+    this.httpTransport = null;
+  }
+
+  SmugMugResponse<SmugMugAlbumInfoResponse> makeAlbumInfoRequest(String url)
+      throws IOException {
+    return makeRequest(url, new TypeReference<SmugMugResponse<SmugMugAlbumInfoResponse>>() {});
+  }
+
+  SmugMugResponse<SmugMugAlbumsResponse> makeAlbumRequest(String url) throws IOException {
+    return makeRequest(url, new TypeReference<SmugMugResponse<SmugMugAlbumsResponse>>() {});
+  }
+
+  SmugMugResponse<SmugMugUserResponse> makeUserRequest(String url) throws IOException {
+    return makeRequest(url, new TypeReference<SmugMugResponse<SmugMugUserResponse>>() {});
+  }
+
+  <T> SmugMugResponse<T> makeRequest(
+      String url, TypeReference<SmugMugResponse<T>> typeReference) throws IOException {
+    HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
+    String signedRequest;
+    try {
+      signedRequest = this.authConsumer.sign(BASE_URL + url + "?_accept=application%2Fjson");
+    } catch (OAuthMessageSignerException
+        | OAuthExpectationFailedException
+        | OAuthCommunicationException e) {
+      throw new IOException("Couldn't get albums", e);
+    }
+    HttpRequest getRequest = requestFactory.buildGetRequest(new GenericUrl(signedRequest));
+    HttpResponse response = getRequest.execute();
+    int statusCode = response.getStatusCode();
+    if (statusCode != 200) {
+      throw new IOException(
+          "Bad status code: " + statusCode + " error: " + response.getStatusMessage());
+    }
+    String result =
+        CharStreams.toString(new InputStreamReader(response.getContent(), Charsets.UTF_8));
+    return MAPPER.readValue(result, typeReference);
+  }
+
+  <T> T postRequest(
+      String url, HttpContent content, Map<String, String> headers, TypeReference<T> typeReference)
+      throws IOException {
+    HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
+
+    String fullUrl = url;
+    if (!fullUrl.contains("://")) {
+      fullUrl = BASE_URL + url;
+    }
+
+    HttpRequest postRequest = requestFactory.buildPostRequest(new GenericUrl(fullUrl), content);
+    HttpHeaders httpHeaders =
+        new HttpHeaders().setAccept("application/json").setContentType("application/json");
+    for (Entry<String, String> entry : headers.entrySet()) {
+      httpHeaders.put(entry.getKey(), entry.getValue());
+    }
+    postRequest.setHeaders(httpHeaders);
+
+    try {
+      postRequest = (HttpRequest) this.authConsumer.sign(postRequest).unwrap();
+    } catch (OAuthMessageSignerException
+        | OAuthExpectationFailedException
+        | OAuthCommunicationException e) {
+      throw new IOException("Couldn't create post request", e);
+    }
+
+    HttpResponse response;
+    try {
+      response = postRequest.execute();
+    } catch (HttpResponseException e) {
+      throw new IOException("Problem making request: " + postRequest.getUrl(), e);
+    }
+    int statusCode = response.getStatusCode();
+    if (statusCode < 200 || statusCode >= 300) {
+      throw new IOException(
+          "Bad status code: " + statusCode + " error: " + response.getStatusMessage());
+    }
+    String result =
+        CharStreams.toString(new InputStreamReader(response.getContent(), Charsets.UTF_8));
+
+    return MAPPER.readValue(result, typeReference);
+  }
+}
---
+++ extensions/data-transfer/portability-data-transfer-smugmug/src/main/java/org/dataportabilityproject/transfer/smugmug/photos/model/SmugMugUrl.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2018 The Data Transfer Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.dataportabilityproject.transfer.smugmug.photos.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/** A JSON representation of a authUrl link in SmugMug */
+public final class SmugMugUrl {
+
+  @JsonProperty("Uri")
+  private String uri;
+
+  @JsonProperty("Locator")
+  private String locator;
+
+  @JsonProperty("LocatorType")
+  private String locatorType;
+
+  @JsonProperty("UriDescription")
+  private String uriDescription;
+
+  @JsonProperty("EndpointType")
+  private String endpointType;
+
+  public String getUri() {
+    return uri;
+  }
+
+  public String getLocator() {
+    return locator;
+  }
+
+  public String getLocatorType() {
+    return locatorType;
+  }
+
+  public String getUriDescription() {
+    return uriDescription;
+  }
+
+  public String getEndpointType() {
+    return endpointType;
+  }
+}
