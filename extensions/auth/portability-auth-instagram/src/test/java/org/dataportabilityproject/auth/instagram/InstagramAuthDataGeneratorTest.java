325e03747d4621411de9318a7644bc329ddfac09
Siham Hussein <seehamrun@users.noreply.github.com>
2018-04-11 12:34:01 -0500

    Implement Instagram Auth (#338)
    Verified authentication flow through UI works as expected. (And fails at
    copy since TransferExtension isn't implemented)

--- extensions/auth/portability-auth-instagram/src/test/java/org/dataportabilityproject/auth/instagram/InstagramAuthDataGeneratorTest.java
+++ extensions/auth/portability-auth-instagram/src/test/java/org/dataportabilityproject/auth/instagram/InstagramAuthDataGeneratorTest.java
@@ -15,26 +15,38 @@
  */
 package org.dataportabilityproject.auth.instagram;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertNull;
 
+import com.google.api.client.http.HttpTransport;
+import org.dataportabilityproject.spi.api.types.AuthFlowConfiguration;
+import org.dataportabilityproject.types.transfer.auth.AppCredentials;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 /** Initial test for GoogleAuthDataGenerator. */
 @RunWith(MockitoJUnitRunner.class)
 public class InstagramAuthDataGeneratorTest {
+  AppCredentials appCredentials = new AppCredentials("dummy-id", "dummy-secret");
+  @Mock private HttpTransport httpTransport;
 
   @Test
   public void generateConfiguration() {
-    assertNull(new InstagramAuthDataGenerator().generateConfiguration(null, null));
+    InstagramAuthDataGenerator generator =
+        new InstagramAuthDataGenerator(appCredentials, httpTransport);
+
+    AuthFlowConfiguration config =
+        generator.generateConfiguration("http://localhost/test", "54321");
+    assertThat(config.getUrl())
+        .isEqualTo(
+            "https://api.instagram.com/oauth/authorize"
+                + "?client_id=dummy-id"
+                + "&redirect_uri=http://localhost/test/callback/instagram"
+                + "&response_type=code"
+                + "&scope=basic"
+                + "&state=NTQzMjE%3D");
   }
-
-  @Test
-  public void generateAuthData() {
-    assertNull(new InstagramAuthDataGenerator().generateAuthData(null, null, null, null, null));
-  }
-
-
-
 }
