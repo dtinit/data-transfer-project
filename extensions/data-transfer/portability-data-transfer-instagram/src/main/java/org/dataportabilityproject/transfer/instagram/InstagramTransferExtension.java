be3ace1e7fe50d3d6eb79c4264d096a9afcffc6a
Siham Hussein <sihamh@google.com>
2018-04-12 13:49:24 -0500

    Delete instagram importer since import is not supported

--- extensions/data-transfer/portability-data-transfer-instagram/src/main/java/org/dataportabilityproject/transfer/instagram/InstagramTransferExtension.java
+++ extensions/data-transfer/portability-data-transfer-instagram/src/main/java/org/dataportabilityproject/transfer/instagram/InstagramTransferExtension.java
@@ -20,16 +20,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.api.client.http.HttpTransport;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
-import java.io.IOException;
 import org.dataportabilityproject.api.launcher.ExtensionContext;
-import org.dataportabilityproject.spi.cloud.storage.AppCredentialStore;
-import org.dataportabilityproject.spi.cloud.storage.JobStore;
 import org.dataportabilityproject.spi.transfer.extension.TransferExtension;
 import org.dataportabilityproject.spi.transfer.provider.Exporter;
 import org.dataportabilityproject.spi.transfer.provider.Importer;
 import org.dataportabilityproject.transfer.instagram.photos.InstagramPhotoExporter;
-import org.dataportabilityproject.transfer.instagram.photos.InstagramPhotoImporter;
-import org.dataportabilityproject.types.transfer.auth.AppCredentials;
 import org.dataportabilityproject.types.transfer.auth.TokensAndUrlAuthData;
 import org.dataportabilityproject.types.transfer.models.photos.PhotosContainerResource;
 import org.slf4j.Logger;
@@ -38,9 +33,7 @@ import org.slf4j.LoggerFactory;
 public class InstagramTransferExtension implements TransferExtension {
   private final Logger logger = LoggerFactory.getLogger(InstagramTransferExtension.class);
   private static final ImmutableList<String> SUPPORTED_DATA_TYPES = ImmutableList.of("photos");
-  // The identifiers in the key management store for the api client key and secret
-  private static final String INSTAGRAM_KEY = "INSTAGRAM_KEY";
-  private static final String INSTAGRAM_SECRET = "INSTAGRAM_SECRET";
+
   private Exporter<TokensAndUrlAuthData, PhotosContainerResource> exporter;
   private Importer<TokensAndUrlAuthData, PhotosContainerResource> importer;
 
@@ -56,15 +49,15 @@ public class InstagramTransferExtension implements TransferExtension {
     Preconditions.checkArgument(
         initialized, "InstagramTransferExtension not initialized. Unable to get Exporter");
     Preconditions.checkArgument(SUPPORTED_DATA_TYPES.contains(transferDataType));
-    return exporter;
+    return null;
   }
 
   @Override
   public Importer<?, ?> getImporter(String transferDataType) {
     Preconditions.checkArgument(
         initialized, "InstagramTransferExtension not initialized. Unable to get Importer");
-    Preconditions.checkArgument(SUPPORTED_DATA_TYPES.contains(transferDataType));
-    return importer;
+    Preconditions.checkArgument(false, "Instagram does not support import");
+    return null;
   }
 
   @Override
@@ -76,25 +69,9 @@ public class InstagramTransferExtension implements TransferExtension {
 
     ObjectMapper mapper =
         new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-    JobStore jobStore = context.getService(JobStore.class);
+
     HttpTransport httpTransport = context.getService(HttpTransport.class);
-    AppCredentials credentials;
-    try {
-      credentials =
-          context
-              .getService(AppCredentialStore.class)
-              .getAppCredentials(INSTAGRAM_KEY, INSTAGRAM_SECRET);
-    } catch (IOException e) {
-      logger.warn(
-          "Error retrieving Instagram Credentials. Did you set {} and {}?",
-          INSTAGRAM_KEY,
-          INSTAGRAM_SECRET);
-      return;
-    }
-
     exporter = new InstagramPhotoExporter(mapper, httpTransport);
-    importer = new InstagramPhotoImporter();
-
     initialized = true;
   }
 }
