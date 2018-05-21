7e747a6cdbd9460fb2f6879f4b6a67b1c6bdbd0d
Brian Willard <bwillard@gmail.com>
2018-05-21 09:38:58 -0500

    Initial integration for Twitter (#424)
    Add the basic framework of a twitter integration, supports importing/exporting pictures

---
+++ extensions/data-transfer/portability-data-transfer-twitter/src/main/java/org/dataportabilityproject.transfer.twitter/TwitterPhotosImporter.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2018 The Data Transfer Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dataportabilityproject.transfer.twitter;
+
+import com.google.api.client.http.InputStreamContent;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.UUID;
+import org.dataportabilityproject.spi.transfer.provider.ImportResult;
+import org.dataportabilityproject.spi.transfer.provider.ImportResult.ResultType;
+import org.dataportabilityproject.spi.transfer.provider.Importer;
+import org.dataportabilityproject.types.transfer.auth.AppCredentials;
+import org.dataportabilityproject.types.transfer.auth.TokenSecretAuthData;
+import org.dataportabilityproject.types.transfer.models.photos.PhotoModel;
+import org.dataportabilityproject.types.transfer.models.photos.PhotosContainerResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import twitter4j.StatusUpdate;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+final class TwitterPhotosImporter
+    implements Importer<TokenSecretAuthData, PhotosContainerResource> {
+  private final Logger logger = LoggerFactory.getLogger(TwitterPhotosImporter.class);
+  private final AppCredentials appCredentials;
+
+  public TwitterPhotosImporter(AppCredentials appCredentials) {
+    this.appCredentials = appCredentials;
+  }
+
+  @Override
+  public ImportResult importItem(UUID jobId, TokenSecretAuthData authData,
+      PhotosContainerResource data) {
+    Twitter twitterApi = TwitterApiWrapper.getInstance(appCredentials, authData);
+    // Twitter doesn't support an 'Albums' concept, so that information is just lost.
+
+    for (PhotoModel image : data.getPhotos()) {
+      try {
+        StatusUpdate update = new StatusUpdate(image.getDescription());
+        InputStreamContent content =
+            new InputStreamContent(null, getImageAsStream(image.getFetchableUrl()));
+        update.media(image.getTitle(), content.getInputStream());
+
+        twitterApi.tweets().updateStatus(update);
+      } catch (IOException | TwitterException e) {
+        logger.warn("problem importing twitter photo", e);
+        return new ImportResult(ResultType.ERROR, e.getMessage());
+      }
+    }
+    return new ImportResult(ResultType.OK);
+  }
+
+  private InputStream getImageAsStream(String urlStr) throws IOException {
+    URL url = new URL(urlStr);
+    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    conn.connect();
+    return conn.getInputStream();
+  }
+}
---
+++ extensions/data-transfer/portability-data-transfer-twitter/src/main/java/org/dataportabilityproject.transfer.twitter/TwitterTransferExtension.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2018 The Data Transfer Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dataportabilityproject.transfer.twitter;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import java.io.IOException;
+import java.util.List;
+import org.dataportabilityproject.api.launcher.ExtensionContext;
+import org.dataportabilityproject.spi.cloud.storage.AppCredentialStore;
+import org.dataportabilityproject.spi.transfer.extension.TransferExtension;
+import org.dataportabilityproject.spi.transfer.provider.Exporter;
+import org.dataportabilityproject.spi.transfer.provider.Importer;
+import org.dataportabilityproject.types.transfer.auth.AppCredentials;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Extension to allow Twitter content to be transferred.
+ *
+ * <p>Currently only photos content to be exported/imported.
+ *
+ * <p>In the future if a social vertical gets defined support should be added for that.
+ */
+public class TwitterTransferExtension implements TransferExtension {
+  private static final List<String> SUPPORTED_TYPES = ImmutableList.of("photos");
+  private static final Logger LOGGER = LoggerFactory.getLogger(TwitterTransferExtension.class);
+  private static final String TWITTER_KEY = "TWITTER_KEY";
+  private static final String TWITTER_SECRET = "TWITTER_SECRET";
+
+  private TwitterPhotosImporter importer;
+  private TwitterPhotosExporter exporter;
+  private boolean initialized = false;
+
+  @Override
+  public String getServiceId() {
+    return "Twitter";
+  }
+
+  @Override
+  public Exporter<?, ?> getExporter(String transferDataType) {
+    Preconditions.checkArgument(
+        initialized, "Trying to call getExporter before initalizing TwitterTransferExtension");
+    Preconditions.checkArgument(
+        SUPPORTED_TYPES.contains(transferDataType),
+        "Export of " + transferDataType + " not supported by Twitter");
+    return exporter;
+  }
+
+  @Override
+  public Importer<?, ?> getImporter(String transferDataType) {
+    Preconditions.checkArgument(
+        initialized, "Trying to call getExporter before initalizing TwitterTransferExtension");
+    Preconditions.checkArgument(
+        SUPPORTED_TYPES.contains(transferDataType),
+        "Export of " + transferDataType + " not supported by Twitter");
+    return importer;
+  }
+
+  @Override
+  public void initialize(ExtensionContext context) {
+    LOGGER.debug("starting twitter initialization");
+    if (initialized) {
+      LOGGER.warn("TwitterTransferExtension already initialized.");
+      return;
+    }
+
+    AppCredentials appCredentials;
+    try {
+      appCredentials =
+          context
+              .getService(AppCredentialStore.class)
+              .getAppCredentials(TWITTER_KEY, TWITTER_SECRET);
+    } catch (IOException e) {
+      LOGGER.warn(
+          "Unable to retrieve client secrets. Did you set {} and {}?", TWITTER_KEY, TWITTER_SECRET);
+      return;
+    }
+
+    exporter = new TwitterPhotosExporter(appCredentials);
+    importer = new TwitterPhotosImporter(appCredentials);
+    initialized = true;
+  }
+}
