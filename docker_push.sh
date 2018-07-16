17f160f6d674b87910423bcbf4608ba95914c43e
Brian Willard <willard@google.com>
2018-07-16 09:58:14 -0500

    Move the docker file building to the build script so that angular will be installed

--- docker_push.sh
+++ docker_push.sh
@@ -2,5 +2,6 @@
 
 #Script used to push new docker images to docker hug by travis.
 
+./gradlew -PcloudType=local :distributions:demo-server:dockerize
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push datatransferproject/demo
