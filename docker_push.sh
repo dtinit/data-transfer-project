05626807b09844c81eb2fca9c1a464086a8040ee
Brian Willard <willard@google.com>
2018-07-17 06:54:19 -0500

    Add some documentation around where command line variables are set.

--- docker_push.sh
+++ docker_push.sh
@@ -3,5 +3,6 @@
 #Script used to push new docker images to docker hug by travis.
 
 ./gradlew -PcloudType=local :distributions:demo-server:dockerize
+# These envionment variable's are set via the Travis UI/CLI
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push datatransferproject/demo
