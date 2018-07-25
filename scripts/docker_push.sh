fc6730b406696656b6cc65da4f4b71a4f9678477
Rachel Tannenbaum <rtannenbaum@google.com>
2018-07-25 11:14:06 -0400

    Remove extra line

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -2,7 +2,6 @@
 
 #Script used to push new docker images to docker hub by travis.
 
-# ./gradlew -PcloudType=local :distributions:demo-server:dockerize
 # These environment variables are set via the Travis UI/CLI
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push datatransferproject/demo
