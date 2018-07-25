010aa58b860981b64ca0e5c99a8d60b3945a64f5
Rachel Tannenbaum <rtannenbaum@google.com>
2018-07-25 09:23:21 -0400

    Move demo image steps into a separate job, currently the deploy is applying to both gradle and angular jobs, we only want it to happen once for the demo image

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#Script used to push new docker images to docker hug by travis.
+#Script used to push new docker images to docker hub by travis.
 
-./gradlew -PcloudType=local :distributions:demo-server:dockerize
-# These envionment variable's are set via the Travis UI/CLI
+# ./gradlew -PcloudType=local :distributions:demo-server:dockerize
+# These environment variables are set via the Travis UI/CLI
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push datatransferproject/demo
