36a96874a318c7d06230dfa963a768c416fbe64c
Rachel Tannenbaum <rtannenbaum@google.com>
2018-07-25 09:30:38 -0400

    Angular tests aren't working, probably broke during client-rest refactor, will open a separate issue for that

--- client-rest/src/app/app.component.spec.ts
+++ client-rest/src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe("AppComponent", () => {
   it(`should have as title "app"`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("does this fail?");
+    expect(app.title).toEqual("app");
   }));
   it("should render title in a h1 tag", async(() => {
     const fixture = TestBed.createComponent(AppComponent);
