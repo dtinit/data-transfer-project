70e009fda109eb8cc4273d04c2b5227003632216
Cyyber Ttechno <kaushal.forex@gmail.com>
2018-07-26 19:13:02 +0530

    Added tests for DataTypesAction (#513)

---
+++ portability-api/src/test/java/org/datatransferproject/api/action/datatype/DataTypesActionTest.java
@@ -0,0 +1,39 @@
+package org.datatransferproject.api.action.datatype;
+
+import org.datatransferproject.spi.api.auth.AuthServiceProviderRegistry;
+import org.datatransferproject.types.client.datatype.DataTypes;
+import org.datatransferproject.types.client.datatype.GetDataTypes;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class DataTypesActionTest {
+
+  @Test
+  public void testGetRequestType() {
+    AuthServiceProviderRegistry registry = mock(AuthServiceProviderRegistry.class);
+    DataTypesAction dataTypesAction = new DataTypesAction(registry);
+
+    Class<GetDataTypes> actual = dataTypesAction.getRequestType();
+    Assert.assertNotEquals(actual, null);
+    Assert.assertEquals(actual, GetDataTypes.class);
+  }
+
+  @Test
+  public void testHandle() {
+    AuthServiceProviderRegistry registry = mock(AuthServiceProviderRegistry.class);
+    Set<String> dataTypes = new HashSet<>(Arrays.asList("contacts", "photos"));
+    when(registry.getTransferDataTypes()).thenReturn(dataTypes);
+    DataTypesAction dataTypesAction = new DataTypesAction(registry);
+
+    GetDataTypes request = mock(GetDataTypes.class);
+    DataTypes actual = dataTypesAction.handle(request);
+    Assert.assertEquals(actual.getDataTypes(), dataTypes);
+  }
+}
