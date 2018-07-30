c4b55df6d5728b69cdc301d48c88a761aa7d1da3
cyyber <kaushal.forex@gmail.com>
2018-07-30 18:25:16 +0530

    Added RouterTestingModule

--- client-rest/src/app/app.component.spec.ts
+++ client-rest/src/app/app.component.spec.ts
@@ -1,8 +1,12 @@
 import { TestBed, async } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from "./app.component";
 describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
