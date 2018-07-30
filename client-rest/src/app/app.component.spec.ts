51c9d918fe7a4ea1414e8dec8d6996d46f4b2474
cyyber <kaushal.forex@gmail.com>
2018-07-30 19:10:10 +0530

    Fix expected title

--- client-rest/src/app/app.component.spec.ts
+++ client-rest/src/app/app.component.spec.ts
@@ -17,10 +17,10 @@ describe("AppComponent", () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title "app"`, async(() => {
+  it(`should have as title "Data Transfer Project Demo"`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("app");
+    expect(app.title).toEqual("Data Transfer Project Demo");
   }));
   it("should render title in a h1 tag", async(() => {
     const fixture = TestBed.createComponent(AppComponent);
