56e5e0dffcd06c2ce0209924a1024d9657498089
cyyber <kaushal.forex@gmail.com>
2018-07-30 20:49:53 +0530

    Fixed Test: Replaced H1 tag with H3 tag and updated expected content

--- client-rest/src/app/app.component.spec.ts
+++ client-rest/src/app/app.component.spec.ts
@@ -22,10 +22,10 @@ describe("AppComponent", () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual("Data Transfer Project Demo");
   }));
-  it("should render title in a h1 tag", async(() => {
+  it("should render title in a h3 tag", async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector("h1").textContent).toContain("Welcome to app!");
+    expect(compiled.querySelector("h3").textContent).toContain("Data Transfer Project Demo");
   }));
 });
