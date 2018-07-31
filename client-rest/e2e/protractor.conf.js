fbb8ed2e599d0a2a92dc29304ceb0a79ddad0a7b
cyyber <kaushal.forex@gmail.com>
2018-07-31 05:56:07 +0530

    Added Chrome Options

--- client-rest/e2e/protractor.conf.js
+++ client-rest/e2e/protractor.conf.js
@@ -9,7 +9,10 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'chrome',
+    chromeOptions: {
+      args: ["--disable-dev-shm-usage", "--headless", "--disable-gpu",  "--no-sandbox", "--disable-extensions"]
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
