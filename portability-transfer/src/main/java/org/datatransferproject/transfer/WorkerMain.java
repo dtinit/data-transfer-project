f999a6481b7a91b938f6e411de502dea02ae3671
Anna Olson <olsona@google.com>
2018-08-15 12:22:35 -0500

    Refactoring logger

--- portability-transfer/src/main/java/org/datatransferproject/transfer/WorkerMain.java
+++ portability-transfer/src/main/java/org/datatransferproject/transfer/WorkerMain.java
@@ -44,7 +44,9 @@ import org.slf4j.LoggerFactory;
  * state is held in {@link JobMetadata}.
  */
 public class WorkerMain {
-  private static final Logger logger = LoggerFactory.getLogger(WorkerMain.class);
+  private static final Logger logger =
+      LoggerFactory
+          .getLogger(WorkerMain.class);
 
   private Worker worker;
 
