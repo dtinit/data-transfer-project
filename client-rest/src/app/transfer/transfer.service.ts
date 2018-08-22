581a4e42fff3eba7ec1332fba217c6d3885f79b4
Rachel Tannenbaum <rtannenbaum@google.com>
2018-08-22 17:28:22 -0500

    Consolidate URL flags in client

--- client-rest/src/app/transfer/transfer.service.ts
+++ client-rest/src/app/transfer/transfer.service.ts
@@ -10,26 +10,26 @@ export class TransferService {
     }
 
     getServices(dataType: string): Observable<TransferServices> {
-        return this.http.get(`${environment.apiUrl}/transfer/services/${dataType}`) as Observable<TransferServices>;
+        return this.http.get(`${environment.apiBaseUrl}/api/transfer/services/${dataType}`) as Observable<TransferServices>;
     }
 
     createTransferJob(createTransferJob: CreateTransferJob): Observable<TransferJob> {
-        return this.http.post(`${environment.apiUrl}/transfer`, createTransferJob) as Observable<TransferJob>;
+        return this.http.post(`${environment.apiBaseUrl}/api/transfer`, createTransferJob) as Observable<TransferJob>;
     }
 
     generateAuthData(generate: GenerateServiceAuthData): Observable<ServiceAuthData> {
-        return this.http.post(`${environment.apiUrl}/transfer/${generate.id}/generate`, generate) as Observable<ServiceAuthData>;
+        return this.http.post(`${environment.apiBaseUrl}/api/transfer/${generate.id}/generate`, generate) as Observable<ServiceAuthData>;
     }
 
     reserveWorker(reserveWorker: ReserveWorker): Observable<string> {
-        return this.http.post(`${environment.apiUrl}/transfer/worker/${reserveWorker.id}`, reserveWorker) as Observable<string>;
+        return this.http.post(`${environment.apiBaseUrl}/api/transfer/worker/${reserveWorker.id}`, reserveWorker) as Observable<string>;
     }
 
     getReservedWorker(getWorker: GetReservedWorker): Observable<ReservedWorker> {
-        return this.http.get(`${environment.apiUrl}/transfer/worker/${getWorker.id}`) as Observable<ReservedWorker>;
+        return this.http.get(`${environment.apiBaseUrl}/api/transfer/worker/${getWorker.id}`) as Observable<ReservedWorker>;
     }
 
     startTransferJob(start: StartTransferJob): Observable<TransferJob> {
-        return this.http.post(`${environment.apiUrl}/transfer/${start.id}/start`, start) as Observable<TransferJob>;
+        return this.http.post(`${environment.apiBaseUrl}/api/transfer/${start.id}/start`, start) as Observable<TransferJob>;
     }
 }
