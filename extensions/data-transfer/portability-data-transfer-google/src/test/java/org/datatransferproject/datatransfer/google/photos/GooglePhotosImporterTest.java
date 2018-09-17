9a425a8b5a9c455579dada7cccd2a870919b15d6
Anna Olson <olsona@olsona2.chi.corp.google.com>
2018-09-17 14:12:51 -0500

    Updating tests, cleaning up a bit

--- extensions/data-transfer/portability-data-transfer-google/src/test/java/org/datatransferproject/datatransfer/google/photos/GooglePhotosImporterTest.java
+++ extensions/data-transfer/portability-data-transfer-google/src/test/java/org/datatransferproject/datatransfer/google/photos/GooglePhotosImporterTest.java
@@ -16,22 +16,24 @@
 package org.datatransferproject.datatransfer.google.photos;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.google.gdata.util.ServiceException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.UUID;
 import org.datatransferproject.cloud.local.LocalJobStore;
+import org.datatransferproject.datatransfer.google.photos.model.GoogleAlbum;
 import org.datatransferproject.datatransfer.google.photos.model.NewMediaItem;
 import org.datatransferproject.datatransfer.google.photos.model.NewMediaItemResult;
 import org.datatransferproject.datatransfer.google.photos.model.NewMediaItemUpload;
 import org.datatransferproject.spi.cloud.storage.JobStore;
 import org.datatransferproject.spi.transfer.types.TempPhotosData;
 import org.datatransferproject.transfer.ImageStreamProvider;
+import org.datatransferproject.types.transfer.models.photos.PhotoAlbum;
 import org.datatransferproject.types.transfer.models.photos.PhotoModel;
 import org.junit.Before;
 import org.junit.Test;
@@ -54,9 +56,13 @@ public class GooglePhotosImporterTest {
   private ImageStreamProvider imageStreamProvider;
   private InputStream inputStream;
 
+  private static final String OLD_ALBUM_ID = "OLD_ALBUM_ID";
+  private static final String NEW_ALBUM_ID = "NEW_ALBUM_ID";
+
   @Before
   public void setUp() throws IOException {
     googlePhotosInterface = mock(GooglePhotosInterface.class);
+
     when(googlePhotosInterface.uploadPhotoContent(Matchers.any(InputStream.class)))
         .thenReturn(UPLOAD_TOKEN);
     when(googlePhotosInterface.makePostRequest(Matchers.anyString(), Matchers.any(), Matchers.any(),
@@ -73,28 +79,51 @@ public class GooglePhotosImporterTest {
   }
 
   @Test
-  public void exportPhoto() throws IOException, ServiceException {
+  public void exportAlbum() throws IOException {
     // Set up
-    String oldAlbumId = "oldAlbumId";
-    String newAlbumId = "newAlbumId";
+    String albumName = "Album Name";
+    String albumDescription = "Album description";
+    PhotoAlbum albumModel = new PhotoAlbum(OLD_ALBUM_ID, albumName, albumDescription);
+
+    GoogleAlbum responseAlbum = new GoogleAlbum();
+    responseAlbum.setId(NEW_ALBUM_ID);
+    when(googlePhotosInterface.createAlbum(Matchers.any(GoogleAlbum.class)))
+        .thenReturn(responseAlbum);
+
+    // Run test
+    googlePhotosImporter.importSingleAlbum(uuid, null, albumModel);
+
+    // Check results
+    ArgumentCaptor<GoogleAlbum> albumArgumentCaptor = ArgumentCaptor.forClass(GoogleAlbum.class);
+    verify(googlePhotosInterface).createAlbum(albumArgumentCaptor.capture());
+    assertEquals(albumArgumentCaptor.getValue().getTitle(), "Copy of " + albumName);
+    assertNull(albumArgumentCaptor.getValue().getId());
+
+    TempPhotosData tempPhotosData = jobStore.findData(uuid, "tempPhotosData", TempPhotosData.class);
+    assertEquals(tempPhotosData.lookupNewAlbumId(OLD_ALBUM_ID), NEW_ALBUM_ID);
+  }
+
+  @Test
+  public void exportPhoto() throws IOException {
+    // Set up
     PhotoModel photoModel = new PhotoModel(PHOTO_TITLE, IMG_URI, PHOTO_DESCRIPTION, JPEG_MEDIA_TYPE,
-        null, oldAlbumId, false);
+        null, OLD_ALBUM_ID, false);
     TempPhotosData tempPhotosData = new TempPhotosData(uuid);
-    tempPhotosData.addAlbumId(oldAlbumId, newAlbumId);
+    tempPhotosData.addAlbumId(OLD_ALBUM_ID, NEW_ALBUM_ID);
     jobStore.create(uuid, "tempPhotosData", tempPhotosData);
 
     // Run test
-    googlePhotosImporter.importSinglePhoto(null, photoModel, uuid);
+    googlePhotosImporter.importSinglePhoto(uuid, null, photoModel);
 
     // Check results
-    // Verify correct methods were called
     verify(imageStreamProvider).get(IMG_URI);
     verify(googlePhotosInterface).uploadPhotoContent(inputStream);
 
-    ArgumentCaptor<NewMediaItemUpload> argument = ArgumentCaptor.forClass(NewMediaItemUpload.class);
-    verify(googlePhotosInterface).createPhoto(argument.capture());
-    assertEquals(argument.getValue().getAlbumId(), newAlbumId);
-    List<NewMediaItem> newMediaItems = argument.getValue().getNewMediaItems();
+    ArgumentCaptor<NewMediaItemUpload> uploadArgumentCaptor = ArgumentCaptor
+        .forClass(NewMediaItemUpload.class);
+    verify(googlePhotosInterface).createPhoto(uploadArgumentCaptor.capture());
+    assertEquals(uploadArgumentCaptor.getValue().getAlbumId(), NEW_ALBUM_ID);
+    List<NewMediaItem> newMediaItems = uploadArgumentCaptor.getValue().getNewMediaItems();
     assertEquals(newMediaItems.size(), 1);
     NewMediaItem mediaItem = newMediaItems.get(0);
     assertEquals(mediaItem.getSimpleMediaItem().getUploadToken(), UPLOAD_TOKEN);
