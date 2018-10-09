fe8b266f482b7ad0bd9fabf508c752501f87d266
Siham Hussein <sihamh@google.com>
2018-10-09 14:54:37 -0500

    Make ReserveWorkerAction return a ReservedWorker response (instead of an empty string)

--- portability-api/src/main/java/org/datatransferproject/api/action/transfer/ReserveWorkerAction.java
+++ portability-api/src/main/java/org/datatransferproject/api/action/transfer/ReserveWorkerAction.java
@@ -9,6 +9,7 @@ import org.datatransferproject.spi.cloud.storage.JobStore;
 import org.datatransferproject.spi.cloud.types.JobAuthorization;
 import org.datatransferproject.spi.cloud.types.PortabilityJob;
 import org.datatransferproject.types.client.transfer.ReserveWorker;
+import org.datatransferproject.types.client.transfer.ReservedWorker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,7 +20,7 @@ import static org.datatransferproject.api.action.ActionUtils.decodeJobId;
 import static org.datatransferproject.spi.cloud.types.JobAuthorization.State.CREDS_AVAILABLE;
 
 /** Reserves a worker to process a transfer job. */
-public class ReserveWorkerAction implements Action<ReserveWorker, String> {
+public class ReserveWorkerAction implements Action<ReserveWorker, ReservedWorker> {
   private static final Logger logger = LoggerFactory.getLogger(ReserveWorkerAction.class);
 
   private final JobStore jobStore;
@@ -35,12 +36,12 @@ public class ReserveWorkerAction implements Action<ReserveWorker, String> {
   }
 
   @Override
-  public String handle(ReserveWorker reserveWorker) {
+  public ReservedWorker handle(ReserveWorker reserveWorker) {
     String id = reserveWorker.getId();
     Preconditions.checkNotNull(id, "transfer job ID required for ReserveWorkerAction");
     UUID jobId = decodeJobId(id);
     updateStateToCredsAvailable(jobId);
-    return "";
+    return new ReservedWorker("");
   }
 
   /**
@@ -50,11 +51,9 @@ public class ReserveWorkerAction implements Action<ReserveWorker, String> {
   private void updateStateToCredsAvailable(UUID jobId) {
     PortabilityJob job = jobStore.findJob(jobId);
     validateJob(job);
-
     // Set update job auth data
     JobAuthorization jobAuthorization =
         job.jobAuthorization().toBuilder().setState(CREDS_AVAILABLE).build();
-
     job = job.toBuilder().setAndValidateJobAuthorization(jobAuthorization).build();
     try {
       jobStore.updateJob(
