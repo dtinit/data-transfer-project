4a7aea2e2e827f83f17ffd5b1a17543ec0af0d46
William Morland <wmorland@fb.com>
2019-11-14 13:21:34 +0000

    google-java-format-1.7

--- portability-types-transfer/src/test/java/org/datatransferproject/types/transfer/auth/AuthDataSerializationTest.java
+++ portability-types-transfer/src/test/java/org/datatransferproject/types/transfer/auth/AuthDataSerializationTest.java
@@ -2,13 +2,10 @@ package org.datatransferproject.types.transfer.auth;
 
 import static org.junit.Assert.*;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import java.io.IOException;
 import java.util.ArrayList;
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,21 +16,23 @@ public class AuthDataSerializationTest {
   @Before
   public void setUp() {
     objectMapper = new ObjectMapper();
-    objectMapper.registerSubtypes(CookiesAndUrlAuthData.class, TokenAuthData.class,
-        TokensAndUrlAuthData.class, TokenSecretAuthData.class);
+    objectMapper.registerSubtypes(
+        CookiesAndUrlAuthData.class,
+        TokenAuthData.class,
+        TokensAndUrlAuthData.class,
+        TokenSecretAuthData.class);
   }
 
   @Test
   public void verifyCookiesAndUrlAuthData() throws IOException {
     final ArrayList<String> cookies = Lists.newArrayList("cookie_1", "cookie_2");
     final String url = "https://www.example.com/auth";
-    final CookiesAndUrlAuthData authData = new CookiesAndUrlAuthData(
-        cookies,
-        url);
+    final CookiesAndUrlAuthData authData = new CookiesAndUrlAuthData(cookies, url);
 
     final String s = objectMapper.writeValueAsString(authData);
     final AuthData readValue = objectMapper.readValue(s, AuthData.class);
-    assertTrue("The read AuthData should be an instance of CookiesAndUrlAuthData",
+    assertTrue(
+        "The read AuthData should be an instance of CookiesAndUrlAuthData",
         readValue instanceof CookiesAndUrlAuthData);
     final CookiesAndUrlAuthData readAuthData = (CookiesAndUrlAuthData) readValue;
     assertEquals("Expect cookies to be the same", cookies, readAuthData.getCookies());
@@ -47,7 +46,8 @@ public class AuthDataSerializationTest {
 
     final String s = objectMapper.writeValueAsString(authData);
     final AuthData readValue = objectMapper.readValue(s, AuthData.class);
-    assertTrue("The read AuthData should be an instance of TokenAuthData",
+    assertTrue(
+        "The read AuthData should be an instance of TokenAuthData",
         readValue instanceof TokenAuthData);
     final TokenAuthData readAuthData = (TokenAuthData) readValue;
     assertEquals("Expect token to be the same", token, readAuthData.getToken());
@@ -62,11 +62,13 @@ public class AuthDataSerializationTest {
 
     final String s = objectMapper.writeValueAsString(authData);
     final AuthData readValue = objectMapper.readValue(s, AuthData.class);
-    assertTrue("The read AuthData should be an instance of TokenAuthData",
+    assertTrue(
+        "The read AuthData should be an instance of TokenAuthData",
         readValue instanceof TokensAndUrlAuthData);
     final TokensAndUrlAuthData readAuthData = (TokensAndUrlAuthData) readValue;
     assertEquals("Expect access token to be the same", accessToken, readAuthData.getAccessToken());
-    assertEquals("Expect refresh token to be the same", refreshToken, readAuthData.getRefreshToken());
+    assertEquals(
+        "Expect refresh token to be the same", refreshToken, readAuthData.getRefreshToken());
     assertEquals("Expect url to be the same", url, readAuthData.getTokenServerEncodedUrl());
   }
 
@@ -78,7 +80,8 @@ public class AuthDataSerializationTest {
 
     final String s = objectMapper.writeValueAsString(authData);
     final AuthData readValue = objectMapper.readValue(s, AuthData.class);
-    assertTrue("The read AuthData should be an instance of TokenAuthData",
+    assertTrue(
+        "The read AuthData should be an instance of TokenAuthData",
         readValue instanceof TokenSecretAuthData);
     final TokenSecretAuthData readAuthData = (TokenSecretAuthData) readValue;
     assertEquals("Expect token to be the same", token, readAuthData.getToken());
