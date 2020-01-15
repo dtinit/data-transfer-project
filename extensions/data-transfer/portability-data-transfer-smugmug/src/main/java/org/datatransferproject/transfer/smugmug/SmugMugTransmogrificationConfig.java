45f69cbb0b55dcad2e16fa9ebb1fd57c6576396e
Pablo Melana <pablomd@google.com>
2020-01-15 11:41:51 -0600

    fix class inheritance modeling for transmogrificationconfigs by overriding methods

--- extensions/data-transfer/portability-data-transfer-smugmug/src/main/java/org/datatransferproject/transfer/smugmug/SmugMugTransmogrificationConfig.java
+++ extensions/data-transfer/portability-data-transfer-smugmug/src/main/java/org/datatransferproject/transfer/smugmug/SmugMugTransmogrificationConfig.java
@@ -4,9 +4,22 @@ import org.datatransferproject.types.common.models.TransmogrificationConfig;
 
 // This class defines transmogrification paramaters for SmugMug imports
 public class SmugMugTransmogrificationConfig extends TransmogrificationConfig {
-	// Smugmug doesn't allow photos to exist outside of a folder
-	private final boolean 	ALBUM_ALLOW_ROOT_PHOTOS				= false;
-	// Album size specified here:
-  	// https://github.com/google/data-transfer-project/pull/805/files
-	private final int       ALBUM_MAX_SIZE						= 5000;
+    // Smugmug doesn't allow photos to exist outside of a folder
+    private final boolean   ALBUM_ALLOW_ROOT_PHOTOS             = false;
+    // Album size specified here:
+    // https://github.com/google/data-transfer-project/pull/805/files
+    private final int       ALBUM_MAX_SIZE                      = 5000;
+
+    /**
+        We need to override the methods
+    */
+    @Override
+    public boolean getAlbumAllowRootPhotos() {
+        return ALBUM_ALLOW_ROOT_PHOTOS;
+    }
+
+    @Override
+    public int getAlbumMaxSize() {
+        return ALBUM_MAX_SIZE;
+    }
 }
