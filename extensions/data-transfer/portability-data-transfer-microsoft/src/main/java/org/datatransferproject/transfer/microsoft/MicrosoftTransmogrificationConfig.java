7b9d5b96d783209ff3dbf177b9a2e47faa83595d
pablo <pablomd@google.com>
2020-01-17 12:03:29 -0600

    Update MicrosoftTransmogrificationConfig.java (#824)
    * Update MicrosoftTransmogrificationConfig.java
    
    remove . from invalid characters
    
    * Update MicrosoftTransmogrificationConfig.java
    
    make class variables static
    
    * Update SmugMugTransmogrificationConfig.java
    
    make class variables static
    
    * Update TransmogrificationConfig.java
    
    make class variables static
    
    Co-authored-by: Chuy <jchavezbaz@gmail.com>

--- extensions/data-transfer/portability-data-transfer-microsoft/src/main/java/org/datatransferproject/transfer/microsoft/MicrosoftTransmogrificationConfig.java
+++ extensions/data-transfer/portability-data-transfer-microsoft/src/main/java/org/datatransferproject/transfer/microsoft/MicrosoftTransmogrificationConfig.java
@@ -8,8 +8,8 @@ public class MicrosoftTransmogrificationConfig extends TransmogrificationConfig 
     OneDrive has forbidden characters for file names:
         https://support.office.com/en-us/article/invalid-file-names-and-file-types-in-onedrive-onedrive-for-business-and-sharepoint-64883a5d-228e-48f5-b3d2-eb39e07630fa#invalidcharacters
     */  
-    private final String    PHOTO_TITLE_FORBIDDEN_CHARACTERS    = "~\"#%&*:<>?/\\{|}.";
-    private final String    ALBUM_NAME_FORBIDDEN_CHARACTERS     = "~\"#%&*:<>?/\\{|}.";
+    private static final String    PHOTO_TITLE_FORBIDDEN_CHARACTERS    = "~\"#%&*:<>?/\\{|}";
+    private static final String    ALBUM_NAME_FORBIDDEN_CHARACTERS     = "~\"#%&*:<>?/\\{|}";
 
     /**
         We need to override the methods
