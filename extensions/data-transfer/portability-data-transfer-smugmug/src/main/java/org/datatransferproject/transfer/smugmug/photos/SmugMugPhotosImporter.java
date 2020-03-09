637c2030cfe75555e39a62b3353556dd9de0dc2a
pablo <pablomd@google.com>
2020-03-09 16:08:55 -0400

    Update SmugMugPhotosImporter.java

--- extensions/data-transfer/portability-data-transfer-smugmug/src/main/java/org/datatransferproject/transfer/smugmug/photos/SmugMugPhotosImporter.java
+++ extensions/data-transfer/portability-data-transfer-smugmug/src/main/java/org/datatransferproject/transfer/smugmug/photos/SmugMugPhotosImporter.java
@@ -122,9 +122,9 @@ public class SmugMugPhotosImporter
   @VisibleForTesting
   SmugMugAlbumResponse importSingleAlbum(PhotoAlbum inputAlbum, SmugMugInterface smugMugInterface)
       throws IOException {
-    Preconditions.checkNotNull(smugMugInterface);
-    Preconditions.checkNotNull(inputAlbum);
-    Preconditions.checkNotNull(inputAlbum.getName());
+    checkNotNull(smugMugInterface);
+    checkNotNull(inputAlbum);
+    checkNotNull(inputAlbum.getName());
     SmugMugAlbumResponse albumResponse = smugMugInterface.createAlbum(inputAlbum.getName());
     monitor.info(() -> "Created an album", albumResponse);
     return albumResponse;
