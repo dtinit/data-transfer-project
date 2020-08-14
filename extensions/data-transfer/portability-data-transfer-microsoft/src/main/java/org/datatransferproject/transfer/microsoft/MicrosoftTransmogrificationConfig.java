6cdbabcc23fd21847cff90df1b00366a344fa72a
seehamrun <sihamh@google.com>
2020-08-13 20:56:52 -0500

    Actually just make the restriction in the ALBUM_FORBIDDEN_CHARACTERS isntead of filtering them all out

--- extensions/data-transfer/portability-data-transfer-microsoft/src/main/java/org/datatransferproject/transfer/microsoft/MicrosoftTransmogrificationConfig.java
+++ extensions/data-transfer/portability-data-transfer-microsoft/src/main/java/org/datatransferproject/transfer/microsoft/MicrosoftTransmogrificationConfig.java
@@ -4,23 +4,23 @@ import org.datatransferproject.types.common.models.TransmogrificationConfig;
 
 // This class defines transmogrification paramaters for Microsoft imports
 public class MicrosoftTransmogrificationConfig extends TransmogrificationConfig {
-    /** 
-    OneDrive has forbidden characters for file names:
-        https://support.office.com/en-us/article/invalid-file-names-and-file-types-in-onedrive-onedrive-for-business-and-sharepoint-64883a5d-228e-48f5-b3d2-eb39e07630fa#invalidcharacters
-    */  
-    private static final String    PHOTO_TITLE_FORBIDDEN_CHARACTERS    = "~\"#%&*:<>?/\\{|}";
-    private static final String    ALBUM_NAME_FORBIDDEN_CHARACTERS     = "~\"#%&*:<>?/\\{|}";
 
-    /**
-        We need to override the methods
-    */
-    @Override
-    public String getPhotoTitleForbiddenCharacters() {
-        return PHOTO_TITLE_FORBIDDEN_CHARACTERS;
-    }
+  /**
+   * OneDrive has forbidden characters for file names: https://support.office.com/en-us/article/invalid-file-names-and-file-types-in-onedrive-onedrive-for-business-and-sharepoint-64883a5d-228e-48f5-b3d2-eb39e07630fa#invalidcharacters
+   */
+  private static final String PHOTO_TITLE_FORBIDDEN_CHARACTERS = "~\"#%&*:<>?/\\{|}";
+  private static final String ALBUM_NAME_FORBIDDEN_CHARACTERS = ".~\"#%&*:<>?/\\{|}";
 
-    @Override
-    public String getAlbumNameForbiddenCharacters() {
-        return ALBUM_NAME_FORBIDDEN_CHARACTERS;
-    }
+  /**
+   * We need to override the methods
+   */
+  @Override
+  public String getPhotoTitleForbiddenCharacters() {
+    return PHOTO_TITLE_FORBIDDEN_CHARACTERS;
+  }
+
+  @Override
+  public String getAlbumNameForbiddenCharacters() {
+    return ALBUM_NAME_FORBIDDEN_CHARACTERS;
+  }
 }
