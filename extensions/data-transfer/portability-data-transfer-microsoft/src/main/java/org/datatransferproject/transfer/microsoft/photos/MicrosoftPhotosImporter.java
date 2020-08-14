7d72d01f41b53f151bb7c1705543d2d8886892c6
Siham Hussein <seehamrun@users.noreply.github.com>
2020-08-14 12:01:45 -0500

    Add period `.` to list of forbidden album name characters (#924)
    * Remove periods from microsoft album names
    
    * Actually just make the restriction in the ALBUM_FORBIDDEN_CHARACTERS isntead of filtering them all out

--- extensions/data-transfer/portability-data-transfer-microsoft/src/main/java/org/datatransferproject/transfer/microsoft/photos/MicrosoftPhotosImporter.java
+++ extensions/data-transfer/portability-data-transfer-microsoft/src/main/java/org/datatransferproject/transfer/microsoft/photos/MicrosoftPhotosImporter.java
@@ -138,7 +138,7 @@ public class MicrosoftPhotosImporter
     // Note that PhotoAlbum.getName() can return an empty string or null depending
     // on the results of PhotoAlbum.cleanName(), e.g. if a Google Photos album has
     // title=" ", its cleaned name will be "". See PhotoAlbum.cleanName for further
-    // details on what forms the name can take .
+    // details on what forms the name can take.
     String albumName = Strings.isNullOrEmpty(album.getName()) ? "Untitled" : album.getName();
     rawFolder.put("name", albumName);
     rawFolder.put("folder", new LinkedHashMap());
