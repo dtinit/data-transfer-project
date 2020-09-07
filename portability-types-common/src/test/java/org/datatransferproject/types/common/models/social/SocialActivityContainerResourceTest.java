4a40cb396f6f418fd7fab6ae4f70ec668c8d852a
mukul-rathi <mukulrathi@fb.com>
2020-09-07 16:55:17 +0100

    Migrate SocialActivityContainerResource from Activity Streams to use DTP data models
    Summary:
    We update the `SocialActivityContainerResource` to use the `SocialActivityModel` instead of AS Activity. We store the actor as separate field, so the information is stored per-container-resource rather than the AS per-activity approach.
    
    We also remove `subContainers` property, as it is not relevant to posts (we can't have nested posts within posts). This field has not been used in the GPlusExporter nor the Mastodon importer/exporter.
    
    Test Plan:
    ./gradlew build
    
    Added tests to check serialisation-deserialisation logic of container.
    
    Reviewers: wmorland, #dtp
    
    Reviewed By: wmorland
    
    Differential Revision: https://phabricator.intern.facebook.com/D23564216
    
    Tasks: T71597511
    
    Tags: data-transfer-project
    
    Signature: 23564216:1599491235:ab80e3ef352479f24f4afdfad39417db6f193855

---
+++ portability-types-common/src/test/java/org/datatransferproject/types/common/models/social/SocialActivityContainerResourceTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2020 The Data Transfer Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.datatransferproject.types.common.models.social;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.google.common.collect.ImmutableList;
+import com.google.common.truth.Truth;
+import org.datatransferproject.types.common.models.ContainerResource;
+import org.junit.Test;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+
+public class SocialActivityContainerResourceTest {
+
+  @Test
+  public void verifySerializeDeserialize() throws Exception {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.registerModule(new JavaTimeModule());
+    objectMapper.registerSubtypes(SocialActivityContainerResource.class);
+
+    Instant timePublished = OffsetDateTime.of(1970, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC).toInstant();
+
+    SocialActivityLocation location = new SocialActivityLocation("Somewhere", 0, 0);
+    SocialActivityAttachment linkAttachment =
+        new SocialActivityAttachment(
+            SocialActivityAttachmentType.LINK, "some url", "some link", "this is the link text");
+    SocialActivityAttachment imageAttachment =
+        new SocialActivityAttachment(
+            SocialActivityAttachmentType.IMAGE,
+            "some image url",
+            "some image",
+            "this is the image description");
+    List<SocialActivityModel> activities =
+        ImmutableList.of(
+            new SocialActivityModel(
+                "id1",
+                timePublished,
+                SocialActivityType.NOTE,
+                null,
+                null,
+                "Write Better tests",
+                "Here's some sample content",
+                "https://facebook.com"),
+            new SocialActivityModel(
+                "id2",
+                timePublished,
+                SocialActivityType.POST,
+                ImmutableList.of(linkAttachment, imageAttachment),
+                null,
+                "Write Some more tests",
+                "Here's some sample post content",
+                "https://facebook.com/dtp"),
+            new SocialActivityModel(
+                "id3",
+                timePublished,
+                SocialActivityType.CHECKIN,
+                null,
+                location,
+                "Write Some location tests",
+                "Here's some sample checkin content",
+                "https://facebook.com/dtp"));
+
+    SocialActivityActor actor =
+        new SocialActivityActor("actor ID", "actor url", "facebook.com/zuck");
+
+    ContainerResource data = new SocialActivityContainerResource("someID", actor, activities);
+
+    String serialized = objectMapper.writeValueAsString(data);
+
+    ContainerResource deserializedModel =
+        objectMapper.readValue(serialized, ContainerResource.class);
+
+    Truth.assertThat(deserializedModel).isNotNull();
+    Truth.assertThat(deserializedModel).isInstanceOf(SocialActivityContainerResource.class);
+    SocialActivityContainerResource deserialized =
+        (SocialActivityContainerResource) deserializedModel;
+    Truth.assertThat(deserialized.getActivities()).hasSize(3);
+    Truth.assertThat(deserialized).isEqualTo(data);
+  }
+}
