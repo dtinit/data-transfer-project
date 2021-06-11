d3612492bc382c9b9aa388d08e20c34a3327f9c7
Vasyl Shchetynin <wshche@gmail.com>
2021-06-11 21:24:33 +0300

    Init commit for photobucket transfer extension

---
+++ extensions/data-transfer/portability-data-transfer-photobucket/src/main/java/org/datatransferproject/transfer/photobucket/PhotobucketTransferExtension.java
@@ -0,0 +1,76 @@
+package org.datatransferproject.transfer.photobucket;
+
+import com.google.common.collect.ImmutableList;
+import okhttp3.OkHttpClient;
+import org.datatransferproject.api.launcher.ExtensionContext;
+import org.datatransferproject.api.launcher.Monitor;
+import org.datatransferproject.spi.cloud.storage.AppCredentialStore;
+import org.datatransferproject.spi.cloud.storage.TemporaryPerJobDataStore;
+import org.datatransferproject.spi.transfer.extension.TransferExtension;
+import com.google.common.base.Preconditions;
+import org.datatransferproject.spi.transfer.provider.Exporter;
+import org.datatransferproject.spi.transfer.provider.Importer;
+import org.datatransferproject.transfer.photobucket.photos.PhotobucketPhotosExporter;
+import org.datatransferproject.transfer.photobucket.photos.PhotobucketPhotosImporter;
+import org.datatransferproject.types.transfer.auth.AppCredentials;
+
+import static java.lang.String.format;
+import static org.datatransferproject.transfer.photobucket.data.PhotobucketConstants.*;
+
+public class PhotobucketTransferExtension implements TransferExtension {
+  private static final ImmutableList<String> SUPPORTED_SERVICES = ImmutableList.of("PHOTOS");
+  private PhotobucketPhotosExporter exporter;
+  private PhotobucketPhotosImporter importer;
+  private boolean initialized = false;
+
+  @Override
+  public String getServiceId() {
+    return PB_SERVICE_ID;
+  }
+
+  @Override
+  public Exporter<?, ?> getExporter(String transferDataType) {
+    Preconditions.checkArgument(initialized);
+    Preconditions.checkArgument(SUPPORTED_SERVICES.contains(transferDataType));
+    return exporter;
+  }
+
+  @Override
+  public Importer<?, ?> getImporter(String transferDataType) {
+    Preconditions.checkArgument(initialized);
+    Preconditions.checkArgument(SUPPORTED_SERVICES.contains(transferDataType));
+    return importer;
+  }
+
+  @Override
+  public void initialize(ExtensionContext context) {
+    Monitor monitor = context.getMonitor();
+    monitor.debug(() -> "Starting PhotobucketTransferExtension initialization");
+    if (initialized) {
+      monitor.severe(() -> "PhotobucketTransferExtension already initialized.");
+      return;
+    }
+    AppCredentials credentials;
+
+    try {
+      credentials =
+          context.getService(AppCredentialStore.class).getAppCredentials(PB_KEY, PB_SECRET);
+    } catch (Exception e) {
+      monitor.info(
+          () ->
+              format(
+                  "Unable to retrieve Photobucket AppCredentials. Did you set %s and %s?",
+                  PB_KEY, PB_SECRET),
+          e);
+      initialized = false;
+      return;
+    }
+
+    OkHttpClient httpClient = context.getService(OkHttpClient.class);
+    TemporaryPerJobDataStore jobStore = context.getService(TemporaryPerJobDataStore.class);
+
+    importer = new PhotobucketPhotosImporter(credentials, monitor, httpClient, jobStore);
+    exporter = new PhotobucketPhotosExporter(credentials, monitor);
+    initialized = true;
+  }
+}
---
+++ extensions/data-transfer/portability-data-transfer-photobucket/src/main/java/org/datatransferproject/transfer/photobucket/data/PhotobucketConstants.java
@@ -0,0 +1,11 @@
+package org.datatransferproject.transfer.photobucket.data;
+
+public final class PhotobucketConstants {
+    public static final String MAIN_ALBUM_TITLE = "Imported media";
+    public static final String ALBUM_TITLE_PREFIX = "Copy of ";
+    public static final String PB_KEY = "PB_KEY";
+    public static final String PB_SECRET = "PB_SECRET";
+    public static final String PB_SERVICE_ID = "Photobucket";
+    public static final String GQL_URL = "https://app.photobucket.com/api/graphql";
+    public static final String UPLOAD_URL = "https://app.photobucket.com/api/upload_by_url";
+}
