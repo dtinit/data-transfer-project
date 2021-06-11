b526f09a7fd371e11f773bc00859ebf668106406
Vasyl Shchetynin <wshche@gmail.com>
2021-06-11 21:24:33 +0300

    Init commit for photobucket transfer extension

---
+++ extensions/data-transfer/portability-data-transfer-photobucket/src/main/java/org/datatransferproject/transfer/photobucket/photos/PhotobucketPhotosExporter.java
@@ -0,0 +1,29 @@
+package org.datatransferproject.transfer.photobucket.photos;
+
+import org.datatransferproject.api.launcher.Monitor;
+import org.datatransferproject.spi.transfer.provider.ExportResult;
+import org.datatransferproject.spi.transfer.provider.Exporter;
+import org.datatransferproject.types.common.ExportInformation;
+import org.datatransferproject.types.common.models.photos.PhotosContainerResource;
+import org.datatransferproject.types.transfer.auth.AppCredentials;
+import org.datatransferproject.types.transfer.auth.AuthData;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class PhotobucketPhotosExporter implements Exporter<AuthData, PhotosContainerResource> {
+  private final Monitor monitor;
+
+  public PhotobucketPhotosExporter(AppCredentials credentials, Monitor monitor) {
+    this.monitor = monitor;
+  }
+
+  @Override
+  public ExportResult<PhotosContainerResource> export(
+      UUID jobId, AuthData authData, Optional<ExportInformation> exportInformation)
+      throws Exception {
+    monitor.severe(
+        () -> "PhotobucketPhotosExporter is not implemented yet, unable to export data.");
+    return null;
+  }
+}
