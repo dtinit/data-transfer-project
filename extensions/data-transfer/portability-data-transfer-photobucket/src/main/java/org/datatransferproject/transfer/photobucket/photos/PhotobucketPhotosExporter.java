580dc72b1d57ec09624e9151c714a05d9184d192
Vasyl Shchetynin <wshche@gmail.com>
2021-06-14 18:07:43 +0300

    Added credentials factory

--- extensions/data-transfer/portability-data-transfer-photobucket/src/main/java/org/datatransferproject/transfer/photobucket/photos/PhotobucketPhotosExporter.java
+++ extensions/data-transfer/portability-data-transfer-photobucket/src/main/java/org/datatransferproject/transfer/photobucket/photos/PhotobucketPhotosExporter.java
@@ -3,9 +3,9 @@ package org.datatransferproject.transfer.photobucket.photos;
 import org.datatransferproject.api.launcher.Monitor;
 import org.datatransferproject.spi.transfer.provider.ExportResult;
 import org.datatransferproject.spi.transfer.provider.Exporter;
+import org.datatransferproject.transfer.photobucket.client.PhotobucketCredentialsFactory;
 import org.datatransferproject.types.common.ExportInformation;
 import org.datatransferproject.types.common.models.photos.PhotosContainerResource;
-import org.datatransferproject.types.transfer.auth.AppCredentials;
 import org.datatransferproject.types.transfer.auth.AuthData;
 
 import java.util.Optional;
@@ -14,7 +14,7 @@ import java.util.UUID;
 public class PhotobucketPhotosExporter implements Exporter<AuthData, PhotosContainerResource> {
   private final Monitor monitor;
 
-  public PhotobucketPhotosExporter(AppCredentials credentials, Monitor monitor) {
+  public PhotobucketPhotosExporter(PhotobucketCredentialsFactory credentialsFactory, Monitor monitor) {
     this.monitor = monitor;
   }
 
