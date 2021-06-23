a18492f75b6efb1c46803514b34b1016ac968da3
Vasyl Shchetynin <wshche@gmail.com>
2021-06-23 17:32:21 +0300

    Added free storage verification

--- extensions/data-transfer/portability-data-transfer-photobucket/src/main/java/org/datatransferproject/transfer/photobucket/data/response/rest/UploadMediaResponse.java
+++
@@ -1,8 +0,0 @@
-package org.datatransferproject.transfer.photobucket.data.response.rest;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class UploadMediaResponse {
-    @JsonProperty("id")
-    public String id;
-}
