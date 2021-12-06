df4415f82c5059d6465de4b6e302008ad3fb52b0
Afnan Navaz <afnannavaz@gmail.com>
2021-12-06 14:30:29 +0530

    Update extensions/auth/portability-auth-daybook/src/main/java/org/datatransferproject/auth/daybook/package-info.java
    Co-authored-by: William Morland <wmorland@fb.com>

--- extensions/auth/portability-auth-daybook/src/main/java/org/datatransferproject/auth/daybook/package-info.java
+++ extensions/auth/portability-auth-daybook/src/main/java/org/datatransferproject/auth/daybook/package-info.java
@@ -13,5 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/** Implements importers and exporters for Facebook services. */
+/** Implements importers and exporters for Daybook services. */
 package org.datatransferproject.auth.daybook;
