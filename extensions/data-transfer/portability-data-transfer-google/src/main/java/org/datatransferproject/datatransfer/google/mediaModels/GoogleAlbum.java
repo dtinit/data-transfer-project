dtinit/data-transfer-project

commit 7dd1e9a09afa787a35c607f3ab28443f64e77f42

[Google] skip missing albums during importPhotoBatch and add debug logging (#1109)
* [Google] skip missing albums during importPhotoBatch and add debug logging

Summary:

Test Plan:

Reviewers:

Subscribers:

Tasks:

Tags:

Differential Revision: https://phabricator.intern.facebook.com/D37539833

* [Google] skip missing albums during importPhotoBatch and add debug logging

Summary:

Test Plan:

Reviewers:

Subscribers:

Tasks:

Tags:

Differential Revision: https://phabricator.intern.facebook.com/D37539833

* rm albumId logging

* fix format

Co-authored-by: William Morland <wmorland@fb.com>

extensions/data-transfer/portability-data-transfer-google/src/main/java/org/datatransferproject/datatransfer/google/mediaModels/GoogleAlbum.java
@@ -28,9 +28,19 @@ public class GoogleAlbum {
   @JsonProperty("title")
   private String title;
 
+  @JsonProperty("isWriteable")
+  private boolean isWriteable;
+
+  @JsonProperty("mediaItemsCount")
+  private long mediaItemsCount;
+
   public String getId() { return id; }
 
   public String getTitle() { return title; }
+
+  public boolean getIsWritable() { return isWriteable; }
+
+  public long getMediaItemsCount() { return mediaItemsCount; }
 
   public void setId(String id) { this.id = id; }
 

extensions/data-transfer/portability-data-transfer-google/src/main/java/org/datatransferproject/datatransfer/google/photos/GooglePhotosImporter.java
@@ -36,6 +36,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.datatransferproject.api.launcher.Monitor;
 import org.datatransferproject.datatransfer.google.common.GoogleCredentialFactory;
@@ -281,15 +282,37 @@ public class GooglePhotosImporter
         }
       }
     } catch (IOException e) {
-      if (e.getMessage() != null
-          && e.getMessage().contains("The remaining storage in the user's account is not enough")) {
+      if (StringUtils.contains(
+          e.getMessage(), "The remaining storage in the user's account is not enough")) {
         throw new DestinationMemoryFullException("Google destination storage full", e);
+      } else if (StringUtils.contains(
+          e.getMessage(), "The provided ID does not match any albums")) {
+        // which means the album was likely deleted by the user
+        // we skip this batch and log some data to understand it better
+        logMissingAlbumDetails(jobId, authData, albumId, e);
       } else {
         throw e;
       }
     }
 
     return totalBytes;
+  }
+
+  private void logMissingAlbumDetails(
+      UUID jobId, TokensAndUrlAuthData authData, String albumId, IOException e) {
+    monitor.info(
+        () -> format("Can't find album during createPhotos call, album is likely deleted"), e);
+    try {
+      GoogleAlbum album = getOrCreatePhotosInterface(jobId, authData).getAlbum(albumId);
+      monitor.debug(
+          () ->
+              format(
+                  "Can't find album during createPhotos call, album info: isWriteable %b, mediaItemsCount %d",
+                  album.getIsWritable(), album.getMediaItemsCount()),
+          e);
+    } catch (Exception ex) {
+      monitor.info(() -> format("Can't find album during getAlbum call"), ex);
+    }
   }
 
   private long processMediaResult(

extensions/data-transfer/portability-data-transfer-google/src/test/java/org/datatransferproject/datatransfer/google/photos/GooglePhotosImporterTest.java
@@ -223,7 +223,8 @@ public class GooglePhotosImporterTest {
     assertFalse(executor.isKeyCached(failedDataId));
     ErrorDetail errorDetail = executor.getErrors().iterator().next();
     assertEquals(failedDataId, errorDetail.id());
-    assertThat(errorDetail.exception(), CoreMatchers.containsString("Media item could not be created."));
+    assertThat(
+        errorDetail.exception(), CoreMatchers.containsString("Media item could not be created."));
   }
 
   @Test
@@ -368,5 +369,80 @@ public class GooglePhotosImporterTest {
         NEW_ALBUM_ID);
     Mockito.verify(jobStore, Mockito.times(0)).removeData(any(), anyString());
     Mockito.verify(jobStore, Mockito.times(1)).getStream(any(), anyString());
+  }
+
+  @Test
+  public void importPhotoFailedToFindAlbum() throws Exception {
+    PhotoModel photoModel =
+        new PhotoModel(
+            PHOTO_TITLE,
+            IMG_URI,
+            PHOTO_DESCRIPTION,
+            JPEG_MEDIA_TYPE,
+            "oldPhotoID1",
+            OLD_ALBUM_ID,
+            true);
+
+    Mockito.when(googlePhotosInterface.uploadPhotoContent(any())).thenReturn("token1", "token2");
+    JobStore jobStore = Mockito.mock(LocalJobStore.class);
+    Mockito.when(jobStore.getStream(any(), any()))
+        .thenReturn(
+            new TemporaryPerJobDataStore.InputStreamWrapper(
+                new ByteArrayInputStream("TestingBytes".getBytes())));
+    googlePhotosImporter =
+        new GooglePhotosImporter(
+            null, jobStore, null, null, googlePhotosInterface, imageStreamProvider, monitor, 1.0);
+    Mockito.when(googlePhotosInterface.createPhotos(any(NewMediaItemUpload.class)))
+        .thenThrow(new IOException("The provided ID does not match any albums"));
+
+    GoogleAlbum responseAlbum = new GoogleAlbum();
+    Mockito.when(googlePhotosInterface.getAlbum(any())).thenReturn(responseAlbum);
+
+    long bytes =
+        googlePhotosImporter.importPhotoBatch(
+            uuid,
+            Mockito.mock(TokensAndUrlAuthData.class),
+            Lists.newArrayList(photoModel),
+            executor,
+            NEW_ALBUM_ID);
+
+    // didn't throw
+    assertEquals(0, bytes);
+  }
+
+  @Test(expected = IOException.class)
+  public void importPhotoCreatePhotosOtherException() throws Exception {
+    PhotoModel photoModel =
+        new PhotoModel(
+            PHOTO_TITLE,
+            IMG_URI,
+            PHOTO_DESCRIPTION,
+            JPEG_MEDIA_TYPE,
+            "oldPhotoID1",
+            OLD_ALBUM_ID,
+            true);
+
+    Mockito.when(googlePhotosInterface.uploadPhotoContent(any())).thenReturn("token1", "token2");
+    JobStore jobStore = Mockito.mock(LocalJobStore.class);
+    Mockito.when(jobStore.getStream(any(), any()))
+        .thenReturn(
+            new TemporaryPerJobDataStore.InputStreamWrapper(
+                new ByteArrayInputStream("TestingBytes".getBytes())));
+    googlePhotosImporter =
+        new GooglePhotosImporter(
+            null, jobStore, null, null, googlePhotosInterface, imageStreamProvider, monitor, 1.0);
+
+    Mockito.when(googlePhotosInterface.createPhotos(any(NewMediaItemUpload.class)))
+        .thenThrow(new IOException("Some other exception"));
+
+    GoogleAlbum responseAlbum = new GoogleAlbum();
+    Mockito.when(googlePhotosInterface.getAlbum(any())).thenReturn(responseAlbum);
+
+    googlePhotosImporter.importPhotoBatch(
+        uuid,
+        Mockito.mock(TokensAndUrlAuthData.class),
+        Lists.newArrayList(photoModel),
+        executor,
+        NEW_ALBUM_ID);
   }
 }

commit 4bd21f3f2e94b1503585dba1fdcfbd8b11d112be

fix typo introduced in #1109

extensions/data-transfer/portability-data-transfer-google/src/main/java/org/datatransferproject/datatransfer/google/mediaModels/GoogleAlbum.java
@@ -38,7 +38,7 @@ public class GoogleAlbum {
 
   public String getTitle() { return title; }
 
-  public boolean getIsWritable() { return isWriteable; }
+  public boolean getIsWriteable() { return isWriteable; }
 
   public long getMediaItemsCount() { return mediaItemsCount; }
 

extensions/data-transfer/portability-data-transfer-google/src/main/java/org/datatransferproject/datatransfer/google/photos/GooglePhotosImporter.java
@@ -308,7 +308,7 @@ public class GooglePhotosImporter
           () ->
               format(
                   "Can't find album during createPhotos call, album info: isWriteable %b, mediaItemsCount %d",
-                  album.getIsWritable(), album.getMediaItemsCount()),
+                  album.getIsWriteable(), album.getMediaItemsCount()),
           e);
     } catch (Exception ex) {
       monitor.info(() -> format("Can't find album during getAlbum call"), ex);