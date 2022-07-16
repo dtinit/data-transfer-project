610f7fae03f32de8d1c500a917504326a04f32ab
Jonathan Zacsh <j@zac.sh>
2022-07-16 16:09:15 -0500

    leave note about overlapping MediaObject and Fileable interfaces

--- portability-types-common/src/main/java/org/datatransferproject/types/common/Fileable.java
+++ portability-types-common/src/main/java/org/datatransferproject/types/common/Fileable.java
@@ -9,6 +9,8 @@ public interface Fileable {
    * version of this file with standard tools on their PC.
    *
    * See https://en.wikipedia.org/wiki/Media_type
+   *
+   * NOTE: this should match {@link MediaObject#getEncodingFormat} behavior.
    */
   String getMimeType();
 }
