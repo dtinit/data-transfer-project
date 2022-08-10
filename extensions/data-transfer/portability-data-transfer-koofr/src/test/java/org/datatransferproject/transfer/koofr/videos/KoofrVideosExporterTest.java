567f4d28ea6d1352da7c32627066f4cb701a7bfb
chaaran <chaaran@fb.com>
2022-08-10 18:00:09 +0100

    Update KoofrVideosExporterTest.java

--- extensions/data-transfer/portability-data-transfer-koofr/src/test/java/org/datatransferproject/transfer/koofr/videos/KoofrVideosExporterTest.java
+++ extensions/data-transfer/portability-data-transfer-koofr/src/test/java/org/datatransferproject/transfer/koofr/videos/KoofrVideosExporterTest.java
@@ -3,7 +3,6 @@ package org.datatransferproject.transfer.koofr.videos;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.google.common.collect.ImmutableList;
