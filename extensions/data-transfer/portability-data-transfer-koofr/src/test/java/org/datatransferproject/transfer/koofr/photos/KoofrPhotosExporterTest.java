12e9f01a74d476fd9a83fcb6f46af20c9cb1271b
chaaran <chaaran@fb.com>
2022-09-08 18:01:22 +0100

    Assert migration to Junit5

--- extensions/data-transfer/portability-data-transfer-koofr/src/test/java/org/datatransferproject/transfer/koofr/photos/KoofrPhotosExporterTest.java
+++ extensions/data-transfer/portability-data-transfer-koofr/src/test/java/org/datatransferproject/transfer/koofr/photos/KoofrPhotosExporterTest.java
@@ -1,7 +1,8 @@
 package org.datatransferproject.transfer.koofr.photos;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
