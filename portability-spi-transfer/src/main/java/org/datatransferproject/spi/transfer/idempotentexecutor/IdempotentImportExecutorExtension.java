df22f1fb3b5bc0484a3e4616dfe34f496a0f69ab
kateyeo <kateyeomans@google.com>
2023-04-04 01:59:40 -0700

    Fix file renames

--- portability-spi-transfer/src/main/java/org/datatransferproject/spi/transfer/idempotentexecutor/IdempotentImportExecutorExtension.java
+++ portability-spi-transfer/src/main/java/org/datatransferproject/spi/transfer/idempotentexecutor/IdempotentImportExecutorExtension.java
@@ -2,7 +2,8 @@ package org.datatransferproject.spi.transfer.idempotentexecutor;
 
 import org.datatransferproject.api.launcher.BootExtension;
 import org.datatransferproject.api.launcher.ExtensionContext;
+import org.datatransferproject.api.launcher.Monitor;
 
-public interface IdempotentExecutor extends BootExtension {
+public interface IdempotentImportExecutorExtension extends BootExtension {
   IdempotentImportExecutor getIdempotentImportExecutor(ExtensionContext extensionContext);
 }
