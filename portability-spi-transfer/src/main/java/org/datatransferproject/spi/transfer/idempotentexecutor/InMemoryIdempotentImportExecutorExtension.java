e468529be2604b90d31b7257bb517628f1490d33
kateyeo <88804869+kateyeo@users.noreply.github.com>
2023-06-14 09:50:00 -0700

    Change retrying executor to singleton (#1249)
    * Change executor to be singleton so that worker and importers/exporters have use same executor
    
    * Update InMemoryIdempotentImportExecutorExtension.java
    
    * Fix build issues
    
    * Add synchronized annotation to executors

--- portability-spi-transfer/src/main/java/org/datatransferproject/spi/transfer/idempotentexecutor/InMemoryIdempotentImportExecutorExtension.java
+++ portability-spi-transfer/src/main/java/org/datatransferproject/spi/transfer/idempotentexecutor/InMemoryIdempotentImportExecutorExtension.java
@@ -8,14 +8,22 @@ import org.datatransferproject.api.launcher.ExtensionContext;
 public class InMemoryIdempotentImportExecutorExtension
     implements IdempotentImportExecutorExtension {
 
+  IdempotentImportExecutor idempotentImportExecutor;
+  IdempotentImportExecutor retryingIdempotentImportExecutor;
   @Override
-  public IdempotentImportExecutor getIdempotentImportExecutor(ExtensionContext extensionContext) {
-    return new InMemoryIdempotentImportExecutor(extensionContext.getMonitor());
+  public synchronized IdempotentImportExecutor getIdempotentImportExecutor(ExtensionContext extensionContext) {
+    if (idempotentImportExecutor == null) {
+      idempotentImportExecutor = new InMemoryIdempotentImportExecutor(extensionContext.getMonitor());
+    }
+    return idempotentImportExecutor;
   }
 
   @Override
-  public IdempotentImportExecutor getRetryingIdempotentImportExecutor(ExtensionContext extensionContext){
-    return new RetryingInMemoryIdempotentImportExecutor(extensionContext.getMonitor(), extensionContext.getSetting("retryLibrary", null));
+  public synchronized IdempotentImportExecutor getRetryingIdempotentImportExecutor(ExtensionContext extensionContext){
+    if(retryingIdempotentImportExecutor == null) {
+      retryingIdempotentImportExecutor = new RetryingInMemoryIdempotentImportExecutor(extensionContext.getMonitor(), extensionContext.getSetting("retryLibrary", null));
+    }
+    return retryingIdempotentImportExecutor;
   }
 
   @Override
