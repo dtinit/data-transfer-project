579090b60963c4546272da76fbf0612db8fdb3ff
osamahan999 <35288172+osamahan999@users.noreply.github.com>
2023-10-18 10:59:11 -0700

    Made MediaMetadata serializable, added a test to confirm (#1282)
    * Make Photo serializable, add a test that confirms it is serializable
    
    * Make Video serializable and add a test that confirms its serializability
    
    * Made media metadata serializable, added a test that confirms serializability
    
    * Set fail-on-unknown-property to true

--- extensions/data-transfer/portability-data-transfer-google/src/test/java/org/datatransferproject/datatransfer/google/mediaModels/MediaMetadataTest.java
+++ extensions/data-transfer/portability-data-transfer-google/src/test/java/org/datatransferproject/datatransfer/google/mediaModels/MediaMetadataTest.java
@@ -1,34 +1,34 @@
 package org.datatransferproject.datatransfer.google.mediaModels;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectOutputStream;
-import java.util.Date;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Optional;
-import org.datatransferproject.types.common.models.photos.PhotoModel;
-import org.datatransferproject.types.common.models.videos.VideoModel;
 import org.junit.Test;
-public class GoogleAlbumTest {
+import static java.lang.String.format;
+
+public class MediaMetadataTest {
   private static final ObjectMapper mapper = new ObjectMapper()
       .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
 
   @Test
-  public void googleAlbum_isSerializable() {
-    String googleAlbumStringJSON = "{\"id\":\"test_id\", \"title\":\"test_title\", \"isWriteable\":true, \"mediaItemsCount\":10}";
+  public void mediaMetadata_isSerializable() {
+    String photoStringJSON = "{\"cameraMake\":\"testMake\", \"cameraModel\":\"testModel\","
+        + "\"focalLength\":\"5.0\", \"apertureFNumber\":\"2.0\", \"isoEquivalent\":\"8.0\", "
+        + "\"exposureTime\":\"testExposureTime\"}";
+    String videoStringJSON = "{\"cameraMake\":\"testMake\", \"cameraModel\":\"testModel\","
+        + "\"fps\": \"30\", \"status\": \"READY\"}";
+    String mediaMetadataStringJSON = format("{\"photo\": %s, \"video\": %s}", photoStringJSON, videoStringJSON);
+
     boolean serializable = true;
     // Turning an object into a byte array can only be done if the class is serializable.
     try {
-      GoogleAlbum googleAlbum = mapper.readValue(googleAlbumStringJSON, GoogleAlbum.class);
+      MediaMetadata mediaMetadata = mapper.readValue(mediaMetadataStringJSON, MediaMetadata.class);
       ByteArrayOutputStream bos = new ByteArrayOutputStream();
       ObjectOutputStream oos = new ObjectOutputStream(bos);
-      oos.writeObject(googleAlbum);
+      oos.writeObject(mediaMetadata);
       oos.flush();
       byte [] data = bos.toByteArray();
     } catch (Exception e ) {
@@ -36,5 +36,4 @@ public class GoogleAlbumTest {
     }
     assertTrue(serializable);
   }
-
 }
