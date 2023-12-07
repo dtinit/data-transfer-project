b97f597f48f63efc5f97786113731798047bcdb0
Osama Hanhan <osamahanhan@google.com>
2023-12-07 12:12:20 -0800

    Added comments

--- extensions/data-transfer/portability-data-transfer-google/src/main/java/org/datatransferproject/datatransfer/google/common/GoogleErrorLogger.java
+++ extensions/data-transfer/portability-data-transfer-google/src/main/java/org/datatransferproject/datatransfer/google/common/GoogleErrorLogger.java
@@ -4,15 +4,15 @@ import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
 import java.io.IOException;
 import java.util.UUID;
-import org.datatransferproject.cloud.google.GoogleJobStore;
+import org.datatransferproject.spi.cloud.storage.JobStore;
 import org.datatransferproject.types.transfer.errors.ErrorDetail;
 
 /**
  * Helper class that provides functionality for creating errors & logging them to data store.
+ * This class logs errors that should appear as failures in the completion email.
  */
 public class GoogleErrorLogger {
-
-  public static void logFailedItemErrors(UUID jobId, ImmutableList<ErrorDetail> errorDetails, GoogleJobStore jobStore) throws IOException {
+  public static void logFailedItemErrors(UUID jobId, ImmutableList<ErrorDetail> errorDetails, JobStore jobStore) throws IOException {
     jobStore.addErrorsToJob(jobId, errorDetails);
   }
 
