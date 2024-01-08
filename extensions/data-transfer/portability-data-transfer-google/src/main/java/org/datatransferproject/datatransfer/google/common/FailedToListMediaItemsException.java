c8ee2b8990a2462641549ce8460ae7bb34699d44
Osama Hanhan <osamahanhan@google.com>
2024-01-08 15:34:47 -0800

    Added a coment for the new exception

--- extensions/data-transfer/portability-data-transfer-google/src/main/java/org/datatransferproject/datatransfer/google/common/FailedToListMediaItemsException.java
+++ extensions/data-transfer/portability-data-transfer-google/src/main/java/org/datatransferproject/datatransfer/google/common/FailedToListMediaItemsException.java
@@ -1,5 +1,9 @@
 package org.datatransferproject.datatransfer.google.common;
 
+/**
+ * FailedToListMediaItemsException is thrown when we try to call PhotosInterface.listMediaItems and are
+ * unsuccessful.
+ */
 public class FailedToListMediaItemsException extends Exception {
     public FailedToListMediaItemsException(String message, Exception cause) {
         super(message, cause);
