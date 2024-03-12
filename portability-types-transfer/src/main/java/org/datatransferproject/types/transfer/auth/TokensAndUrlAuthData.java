c3a9447d966420aa8850c7d79489adbee4fedb95
Jonathan Zacsh <j@zac.sh>
2024-03-12 17:54:00 -0500

    noop(new api) use `URI`, avoid primitive obsession
    open the door to let us start moving away from "primitive obsession"
    (strings, all strings), which _in turn_ lets us start handling errors
    more concisely and int he right places.
    
    this is a prefactor for an upcoming apple-music PR that relies on this
    class for oauth token refresh.

--- portability-types-transfer/src/main/java/org/datatransferproject/types/transfer/auth/TokensAndUrlAuthData.java
+++ portability-types-transfer/src/main/java/org/datatransferproject/types/transfer/auth/TokensAndUrlAuthData.java
@@ -1,9 +1,16 @@
 package org.datatransferproject.types.transfer.auth;
 
+import static com.google.common.base.Strings.isNullOrEmpty;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /** Token-based authentication data. */
 @JsonTypeName("org.dataportability:TokensAndUrlAuthData")
@@ -35,8 +42,30 @@ public class TokensAndUrlAuthData extends AuthData {
   }
 
   @JsonIgnore
+  public URI getTokenServerEncodedUri() throws IllegalStateException {
+    final String urlString = getTokenServerEncodedUrl();
+    // TODO(jzacsh, hgandhi90) this should probably happen at construction time; maybe there's a jackson annotation for this?
+    checkState(
+        !isNullOrEmpty(urlString),
+        "malformed construction TokensAndUrlAuthData getTokenServerEncodedUrl() should be non-empty, but got \"%s\"",
+        urlString);
+    try {
+      return new URI(urlString);
+    } catch (URISyntaxException e) {
+      throw new IllegalStateException(
+          String.format("TokensAndUrlAuthData built with a malformed token-refresh URI (\"%s\")", urlString),
+          e);
+    }
+  }
+
+  @JsonIgnore
   @Override
   public String getToken() {
     return getAccessToken();
+  }
+
+  @JsonIgnore
+  public TokensAndUrlAuthData rebuildWithRefresh(String fresherAccessToken) {
+    return new TokensAndUrlAuthData(fresherAccessToken, getRefreshToken(), getTokenServerEncodedUrl());
   }
 }
