3486e4eb864935c971843e6d65e20dc59a441966
Jonathan Zacsh <j@zac.sh>
2024-03-12 17:57:06 -0500

    noop(prefactor) new Apple api for token refresh
    common pattern in apple server-API classes. While done differently
    between MediaInterface and MusicInterface, they're fundamentlaly the
    same. Harshil and I discussed and plan to set this up for Music and move
    Media over to this later in our copious spare time.

---
+++ extensions/data-transfer/portability-data-transfer-apple/src/main/java/org/datatransferproject/datatransfer/apple/http/TokenRefresher.java
@@ -0,0 +1,53 @@
+package org.datatransferproject.datatransfer.apple.http;
+
+import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.base.Strings.isNullOrEmpty;
+
+import java.net.URI;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+import org.datatransferproject.types.transfer.auth.AppCredentials;
+import org.datatransferproject.types.transfer.auth.TokensAndUrlAuthData;
+
+// DO NOT MERGE unit test
+public class TokenRefresher {
+  private TokenRefresher() {} // prevent instantiation
+
+  // TODO(jzacsh, hgandhi90) refactor refresh-token flow in AppleMediaInterface to use this codepath
+  // instead of what it has now.
+  public static HttpRequest.Builder buildRefreshRequestUrlForAccessToken(
+      TokensAndUrlAuthData authData, AppCredentials appCredentials) throws IllegalStateException {
+    BodyPublisher postBody = buildRefreshRequestPostBody(authData, appCredentials);
+    URI refreshUri = authData.getTokenServerEncodedUri();
+    return HttpRequest.newBuilder().uri(refreshUri).POST(postBody);
+  }
+
+  private static BodyPublisher buildRefreshRequestPostBody(
+      TokensAndUrlAuthData authData, AppCredentials appCredentials) throws IllegalStateException {
+    final Map<String, String> parameters = new HashMap<>();
+    parameters.put("client_id", checkString(appCredentials.getKey(), "AppCredentials#getKey"));
+    parameters.put(
+        "client_secret", checkString(appCredentials.getSecret(), "AppCredentials#getSecret"));
+    parameters.put("grant_type", "refresh_token");
+    parameters.put(
+        "refresh_token",
+        checkString(authData.getRefreshToken(), "TokensAndUrlAuthData#getRefreshToken"));
+
+    StringJoiner sj = new StringJoiner("&");
+    for (Map.Entry<String, String> entry : parameters.entrySet()) {
+      sj.add(entry.getKey() + "=" + entry.getValue());
+    }
+    return HttpRequest.BodyPublishers.ofString(sj.toString());
+  }
+
+  /** Checks a {@code content} is not null or empty and passes it through. */
+  private static String checkString(String content, String erroMessageTitle)
+      throws IllegalStateException {
+    checkState(
+        !isNullOrEmpty(content), "require non-empty %s, but got \"%s\"", erroMessageTitle, content);
+    return content;
+  }
+}
