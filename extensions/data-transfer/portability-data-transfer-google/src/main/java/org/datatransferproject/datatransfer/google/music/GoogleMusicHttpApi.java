945a9684b4e1ea7a95e41f23ee3eaef1c023b191
Siyu Gao <siyug@google.com>
2024-05-08 16:21:39 +0000

    Reduce read time out for music post request based on Music Import APIs latency

--- extensions/data-transfer/portability-data-transfer-google/src/main/java/org/datatransferproject/datatransfer/google/music/GoogleMusicHttpApi.java
+++ extensions/data-transfer/portability-data-transfer-google/src/main/java/org/datatransferproject/datatransfer/google/music/GoogleMusicHttpApi.java
@@ -192,7 +192,7 @@ public class GoogleMusicHttpApi {
     HttpRequest postRequest =
         requestFactory.buildPostRequest(
             new GenericUrl(baseUrl + "?" + generateParamsString(parameters)), httpContent);
-    postRequest.setReadTimeout(2 * 60000); // 2 minutes read timeout
+    postRequest.setReadTimeout(6000); // 6 seconds read timeout
     HttpResponse response;
 
     try {
