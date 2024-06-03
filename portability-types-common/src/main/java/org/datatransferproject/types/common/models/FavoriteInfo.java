adf07d5c7e1d296cc4c20980cd42d996a0341ea7
akshaysinghh <akshaysinghh@google.com>
2024-06-03 18:36:35 +0000

    Add FavoriteInfo To Photo and Video models

---
+++ portability-types-common/src/main/java/org/datatransferproject/types/common/models/FavoriteInfo.java
@@ -0,0 +1,39 @@
+package org.datatransferproject.types.common.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
+import java.util.Date;
+
+/** FavoriteInfo about a {@code MediaItem}. */
+public class FavoriteInfo implements Serializable {
+
+  @JsonProperty("favorite")
+  private boolean favorited;
+
+  /* the most recent time when the favorite state was changed. */
+  @JsonProperty("lastUpdateTime")
+  private Date lastUpdateTime;
+
+  public FavoriteInfo(boolean favorited, Date lastUpdateTime)
+  {
+    this.favorited = favorited;
+    this.lastUpdateTime = lastUpdateTime;
+  }
+
+  /* whether is favorited as of the last updated time */
+  public boolean getFavorited() {
+    return favorited;
+  }
+
+  public Date getLastUpdateTime() {
+    return lastUpdateTime;
+  }
+
+  public void setFavorited(boolean favorited) {
+    this.favorited = favorited;
+  }
+
+  public void setLastUpdateTime(Date lastUpdateTime) {
+    this.lastUpdateTime = lastUpdateTime;
+  }
+}
