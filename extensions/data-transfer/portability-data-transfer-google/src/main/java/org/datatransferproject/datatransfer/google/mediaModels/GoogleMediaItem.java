9cc5689ffebc90addc155f524bc84d9c59f64ee6
aksingh737 <130187841+aksingh737@users.noreply.github.com>
2024-06-10 15:28:01 -0700

    Add support for favorite state metadata for photos and videos models (#1360)
    * First changelist description
    
    * Fixes upload time bug. Upload time wasn't being populated
    
    * remove unused code
    
    * remove en secrets
    
    * remove useless changelist
    
    * adressed the comments on the changelist
    
    * fix tests
    
    * Correct the idempotentId for videos which prevented the duplicated videos showing in other albums.
    
    * refactor the test to verify that the correct idempotent id is set
    
    * Return dataID for VideoModels when getDataID is called
    
    * Refactor test to check for idempotentId in the executor
    
    * Refactor AppleVideosImporterTest to use idempotentId for videos assertion
    
    * nitfix:indent
    
    * Add FavoriteInfo To Photo and Video models
    
    * Add a default constructor for FavoriteInfo
    
    * Add methods to clarify favoriteInfo population for when the information is either not available or if we want to set it to some initial default state
    
    * Use the correct method to populate favorite info. Use unfavoritedAt method instead of unknown when uploaded time is known
    
    ---------
    
    Co-authored-by: kateyeo <88804869+kateyeo@users.noreply.github.com>

--- extensions/data-transfer/portability-data-transfer-google/src/main/java/org/datatransferproject/datatransfer/google/mediaModels/GoogleMediaItem.java
+++ extensions/data-transfer/portability-data-transfer-google/src/main/java/org/datatransferproject/datatransfer/google/mediaModels/GoogleMediaItem.java
@@ -30,6 +30,7 @@ import java.util.Date;
 import java.util.Optional;
 import org.datatransferproject.types.common.models.photos.PhotoModel;
 import org.datatransferproject.types.common.models.videos.VideoModel;
+import org.datatransferproject.types.common.models.FavoriteInfo;
 import org.apache.tika.Tika;
 import com.google.common.base.Strings;
 
@@ -68,6 +69,9 @@ public class GoogleMediaItem implements Serializable {
   //  services use to display the photos timeline, instead of uploadTime.
   private Date uploadedTime;
 
+  @JsonProperty("favoriteInfo")
+  private FavoriteInfo favoriteInfo;
+
   public boolean isPhoto() {
     return this.getMediaMetadata().getPhoto() != null;
   }
@@ -99,7 +103,8 @@ public class GoogleMediaItem implements Serializable {
         mediaItem.getId(),
         albumId.orElse(null),
         false /*inTempStore*/,
-        getCreationTime(mediaItem));
+        getCreationTime(mediaItem),
+        new FavoriteInfo(getFavorite(mediaItem), getLastUpdateTime(mediaItem)));
   }
 
   public static PhotoModel convertToPhotoModel (
@@ -115,7 +120,8 @@ public class GoogleMediaItem implements Serializable {
         albumId.orElse(null),
         false  /*inTempStore*/,
         null  /*sha1*/,
-        getCreationTime(mediaItem));
+        getCreationTime(mediaItem),
+        new FavoriteInfo(getFavorite(mediaItem), getLastUpdateTime(mediaItem)));
   }
 
   /**
@@ -234,5 +240,17 @@ public class GoogleMediaItem implements Serializable {
 
   public void setUploadedTime(Date date) {
     this.uploadedTime = date;
+  }
+
+  public void setFavoriteInfo(FavoriteInfo favoriteInfo) {
+    this.favoriteInfo = favoriteInfo;
+  }
+  public static boolean getFavorite(GoogleMediaItem mediaItem)  {
+    return mediaItem.favoriteInfo != null && mediaItem.favoriteInfo.getFavorited();
+  }
+
+  public static Date getLastUpdateTime(GoogleMediaItem mediaItem) throws ParseException  {
+    return (mediaItem.favoriteInfo == null  || mediaItem.favoriteInfo.getLastUpdateTime() == null)
+        ? getCreationTime(mediaItem) : mediaItem.favoriteInfo.getLastUpdateTime();
   }
 }
