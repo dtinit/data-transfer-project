5d1fec036c60e9c16f7b204041656ad785ae564a
Jonathan Zacsh <zacsh@google.com>
2024-11-07 16:32:17 -0600

    noop(doc) resolve remaining blockers to PR review

--- portability-spi-api/src/main/java/org/datatransferproject/spi/api/transport/UrlGetStreamer.java
+++ portability-spi-api/src/main/java/org/datatransferproject/spi/api/transport/UrlGetStreamer.java
@@ -9,20 +9,15 @@ import java.net.URL;
 import org.datatransferproject.types.common.DownloadableItem;
 
 /** Implements a simple HTTP GET against a URL to stream the results. */
-/* DO NOT MERGE - update Smugmug's codebase to implement their own, since they have special
- * getImageAsStream impl that isn't a simple GET: https://github.com/dtinit/data-transfer-project/blob/9723399b5b4a66ab431822b2a95f45e6d3380b32/extensions/data-transfer/portability-data-transfer-smugmug/src/main/java/org/datatransferproject/transfer/smugmug/photos/SmugMugPhotosImporter.java#L136 */
+// TODO(techdebt) update Smugmug's codebase to implement their own, since they have special
+// getImageAsStream impl that isn't a simple GET:
+// https://github.com/dtinit/data-transfer-project/blob/9723399b5b4a66ab431822b2a95f45e6d3380b32/extensions/data-transfer/portability-data-transfer-smugmug/src/main/java/org/datatransferproject/transfer/smugmug/photos/SmugMugPhotosImporter.java#L136
+// This will let the DTP codebase share test patterns across adapters.
 public class UrlGetStreamer implements RemoteFileStreamer {
   public InputStream get(String remoteUrl) throws IOException {
     return new BufferedInputStream(new URL(remoteUrl).openStream());
   }
 
-  /* DO NOT MERGE- replace current usages (like Msofts's another others) with this. */
-  /* DO NOT MERGE- maybe this belongs on RemoteFileStreamer too? (just not with the default
-   * impl? */
-
-  /* DO NOT MERGE - delete Flickr's test-double interface in favor of this
-   * https://github.com/dtinit/data-transfer-project/blob/9723399b5b4a66ab431822b2a95f45e6d3380b32/extensions/data-transfer/portability-data-transfer-flickr/src/main/java/org/datatransferproject/datatransfer/flickr/media/FlickrMediaImporter.java#L328
-   * */
   public InputStream get(DownloadableItem downloadableItem) throws IOException {
     checkState(
         downloadableItem.getFetchableUrl() != null,
