d663dad9a9818f8d21b1b9f5a402e4f5ea64d55c
Jonathan Zacsh <zacsh@google.com>
2024-12-04 20:48:37 -0600

    simplify recovery analysis for callers, include example usage in javadoc to show this (want to show we cannot get into more scenarios like we have today with unchecked else-blocks

--- extensions/data-transfer/portability-data-transfer-microsoft/src/main/java/org/datatransferproject/transfer/microsoft/MicrosoftApiResponse.java
+++ extensions/data-transfer/portability-data-transfer-microsoft/src/main/java/org/datatransferproject/transfer/microsoft/MicrosoftApiResponse.java
@@ -23,8 +23,30 @@ import java.util.Optional;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 import org.datatransferproject.spi.transfer.types.DestinationMemoryFullException;
+import org.datatransferproject.spi.transfer.types.PermissionDeniedException;
 
-/** API response from Microsoft API servers. */
+/**
+ * API response from Microsoft API servers.
+ *
+ * <p>Example usage error handling workflow:
+ *
+ * <pre>
+ * MicrosoftApiResponse resp = someServerCall();
+ * Optional<RecoverableState> recovery = resp.recoverableState();
+ * if (recovery.isPresent()) {
+ *   switch (recovery) {
+ *     case RECOVERABLE_STATE_OKAY:
+ *       return; // no errors to handle! we're all set
+ *     case RECOVERABLE_STATE_NEEDS_TOKEN_REFRESH:
+ *        // ... run a token-refresh and trigger your retry control-flow
+ *     default:
+ *       throw new AssertionError("exhaustive switch");
+ *   }
+ * } else {
+ *   throw resp.toDtpException();
+ * }
+ * </pre>
+ */
 @AutoValue
 public abstract class MicrosoftApiResponse {
   /** HTTP status code. */
@@ -33,16 +55,21 @@ public abstract class MicrosoftApiResponse {
   /** HTTP status message. */
   public abstract String httpMessage();
 
-  /** HTTP body of the response. */
-  public abstract ResponseBody body();
+  /** HTTP body of the response if any was present. */
+  public abstract Optional<ResponseBody> body();
 
   /** Builds from key fields within an HTTP response. */
   public static MicrosoftApiResponse ofResponse(Response response) {
-    return MicrosoftApiResponse.builder()
-        .setHttpStatus(response.code())
-        .setBody(response.body())
-        .setHttpMessage(response.message())
-        .build();
+    Builder builder =
+        MicrosoftApiResponse.builder()
+            .setHttpMessage(response.message())
+            .setHttpStatus(response.code());
+
+    if (response.body() != null) {
+      builder.setBody(response.body());
+    }
+
+    return builder.build();
   }
 
   @AutoValue.Builder
@@ -60,35 +87,99 @@ public abstract class MicrosoftApiResponse {
     return new org.datatransferproject.transfer.microsoft.AutoValue_MicrosoftApiResponse.Builder();
   }
 
-  /**
-   * Maps any error state of this response to a DTP-standard exception, if the response seems OK.
-   *
-   * <p>DTP exceptions are usually found in {@link org.datatransferproject.spi.transfer.types}.
-   */
-  public Optional<Exception> toDtpException() {
-    if (isOkay()) {
-      return Optional.empty();
-    }
-
-    if (isDestinationFull()) {
-      return Optional.of(
-          new DestinationMemoryFullException(
-              "Microsoft destination storage limit reached", toIoException()));
-    }
-
-    /* DO NOT MERGE - collect all the cases. */
-
-    return Optional.of(toIoException("unrecognized class of Microsoft API error"));
+  private enum RecoverableState {
+    RECOVERABLE_STATE_OKAY,
+    RECOVERABLE_STATE_NEEDS_TOKEN_REFRESH,
   }
 
-  private boolean isOkay() {
+  private enum FatalState {
+    FATAL_STATE_FATAL_PERMISSION_DENIED,
+    FATAL_STATE_FATAL_DESTINATION_FULL,
+    FATAL_STATE_FATAL_UNSPECIFIED
+  }
+
+  /**
+   * Returns the recoverable state of this response, if there is one, otherwise call {@link
+   * toFatalState} or {@link toDtpException} should detail what's wrong.
+   */
+  public Optional<RecoverableState> recoverableState() {
+    if (isOkay()) {
+      return Optional.of(RecoverableState.RECOVERABLE_STATE_OKAY);
+    } else if (isTokenRefreshRequired()) {
+      return Optional.of(RecoverableState.RECOVERABLE_STATE_NEEDS_TOKEN_REFRESH);
+    } else {
+      return Optional.empty(); // unrecoverable state
+    }
+  }
+
+  /** Whether response indicates you might be able to keep going. */
+  private boolean hasRecovery() {
+    return recoverableState().isPresent();
+  }
+
+  /** Whether the response was OK, otherwise {@link toDtpException} should return something. */
+  public boolean isOkay() {
     return httpStatus() >= 200 && httpStatus() <= 299;
   }
 
-  /** Translate this response body to DTP's base-level exception. */
+  /**
+   * Whether the response health is neither OK nor outright error, but merely requires a token
+   * refresh (then a retry).
+   */
+  private boolean isTokenRefreshRequired() {
+    return httpStatus() == 401;
+  }
+
+  /** Whether response is of an unrecoverable error, indicating {@link toDtpException} usage. */
+  private boolean isFatal() {
+    return !hasRecovery();
+  }
+
+  /**
+   * Maps current error state of this response to a DTP-standard exception, if the response seems OK
+   * (note: this is nonsensical to call when not {@link isFatal}).
+   *
+   * <p>DTP exceptions are usually found in {@link org.datatransferproject.spi.transfer.types}. The
+   * base "cause" of exceptions constructed here will always at least be a read-out of the API
+   * response itself, and possible some more explanatory text.
+   *
+   * <p>Throws IllegalStateException if {@link isFatal} is false.
+   */
+  public Exception toDtpException() {
+    switch (toFatalState()) {
+      case FATAL_STATE_FATAL_PERMISSION_DENIED:
+        return new PermissionDeniedException(
+            "User access to microsoft onedrive was denied", toIoException());
+      case FATAL_STATE_FATAL_DESTINATION_FULL:
+        return new DestinationMemoryFullException(
+            "Microsoft destination storage limit reached", toIoException());
+      case FATAL_STATE_FATAL_UNSPECIFIED:
+        return toIoException("unrecognized class of Microsoft API error");
+      default:
+        throw new AssertionError("exhaustive switch");
+    }
+  }
+
+  private FatalState toFatalState() {
+    checkState(
+        recoverableState().isEmpty(), "cannot explain fatal state when is apparently recoverable");
+    if (httpStatus() == 403 && httpMessage().contains("Access Denied")) {
+      return FatalState.FATAL_STATE_FATAL_PERMISSION_DENIED;
+    }
+    // TODO(zacsh) fix destination full check, it's currently the wrong string.
+    if (httpStatus() == 507 && httpMessage().contains("Insufficient Storage")) {
+      return FatalState.FATAL_STATE_FATAL_DESTINATION_FULL;
+    }
+    return FatalState.FATAL_STATE_FATAL_UNSPECIFIED;
+  }
+
+  /**
+   * Translate this response body to DTP's base-level exception with a cause message that includes
+   * all the details we have.
+   */
   private IOException toIoException() {
-    checkState(!isOkay(), "isOkay(), so no exception to construct");
-    return new IOException(toString());
+    checkState(!hasRecovery(), "isOkay(), so no exception to construct");
+    return new IOException(toString()); // AutoValue toString() includes all fields' values
   }
 
   /**
@@ -96,12 +187,7 @@ public abstract class MicrosoftApiResponse {
    *     construction.
    */
   private IOException toIoException(String message) {
-    checkState(!isOkay(), "isOkay(), so no exception to construct");
+    checkState(!hasRecovery(), "isOkay(), so no exception to construct");
     return new IOException(String.format("%s: %s", message, toString()));
-  }
-
-  // TODO(zacsh) fix this, it's currently the wrong string.
-  private boolean isDestinationFull() {
-    return httpStatus() == 507 && httpMessage().contains("Insufficient Storage");
   }
 }
