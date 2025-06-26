e3a13926613003ee7dc47db7a56bd7c005e446b7
ehung <ehung@synology.com>
2025-06-26 17:23:07 +0800

    feat(models): add config synology.yaml and corresponding data models

---
+++ extensions/data-transfer/portability-data-transfer-synology/src/main/java/org/datatransferproject/datatransfer/synology/utils/UrlUtils.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2025 The Data Transfer Project Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.datatransferproject.datatransfer.synology.utils;
+
+import javax.annotation.Nonnull;
+
+public class UrlUtils {
+  public static String join(@Nonnull String baseUrl, @Nonnull String path) {
+    if (baseUrl.isEmpty()) {
+      throw new IllegalArgumentException("Base URL cannot be empty");
+    }
+
+    boolean baseEndsWithSlash = baseUrl.endsWith("/");
+    boolean pathStartsWithSlash = path.startsWith("/");
+
+    if (baseEndsWithSlash && pathStartsWithSlash) {
+      return baseUrl + path.substring(1);
+    } else if (!baseEndsWithSlash && !pathStartsWithSlash) {
+      return baseUrl + "/" + path;
+    } else {
+      return baseUrl + path;
+    }
+  }
+}
