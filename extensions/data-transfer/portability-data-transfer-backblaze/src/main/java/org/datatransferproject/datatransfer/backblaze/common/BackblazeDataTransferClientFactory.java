b6ae6532a6f9d67de5851b7d65fc95e98ada4625
Anthony Ross <anthony.ross@backblaze.com>
2025-07-11 04:04:51 -0500

    fix: Update Backblaze Client to Request Account Info (#1462)
    Currently the Backblaze client is hard coded to only use a few clusters.
    This change updates the init function to request the account info which
    will include the s3 base URI which we will use for all other requests.
    
    ---------
    
    Co-authored-by: Eric Ding <99442664+eding-backblaze@users.noreply.github.com>
    Co-authored-by: Ameya Shendre <52448570+ameya9@users.noreply.github.com>

--- extensions/data-transfer/portability-data-transfer-backblaze/src/main/java/org/datatransferproject/datatransfer/backblaze/common/BackblazeDataTransferClientFactory.java
+++ extensions/data-transfer/portability-data-transfer-backblaze/src/main/java/org/datatransferproject/datatransfer/backblaze/common/BackblazeDataTransferClientFactory.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.datatransferproject.api.launcher.Monitor;
 import org.datatransferproject.datatransfer.backblaze.exception.BackblazeCredentialsException;
 import org.datatransferproject.transfer.JobMetadata;
@@ -48,7 +49,7 @@ public class BackblazeDataTransferClientFactory {
                       SIZE_THRESHOLD_FOR_MULTIPART_UPLOAD,
                       PART_SIZE_FOR_MULTIPART_UPLOAD);
       String exportService = JobMetadata.getExportService();
-      backblazeDataTransferClient.init(authData.getToken(), authData.getSecret(), exportService);
+      backblazeDataTransferClient.init(authData.getToken(), authData.getSecret(), exportService, HttpClientBuilder.create().build());
       backblazeDataTransferClientMap.put(jobId, backblazeDataTransferClient);
     }
     return backblazeDataTransferClientMap.get(jobId);
